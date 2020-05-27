c6cdaea61cc6cc0b280f08af5760ae06c5bf877d
trentmc <5305452+trentmc@users.noreply.github.com>
2020-05-27 17:21:16 +0200

    fix some bugs

--- deploy.py
+++ deploy.py
@@ -4,13 +4,10 @@ import os
 import sys
 
 import brownie
-import pdb; pdb.set_trace()
-from brownie import ERC20Template, Factory
 import os
 
-from ocean_lib import Ocean
-
 ALLOWED_NETWORKS = ["development", "mainnet", "rinkeby"] #see 'brownie network lists'
+ALLOWED_NETWORKS_STR = str(ALLOWED_NETWORKS)[1:-1]
     
 if __name__ == '__main__':
 
@@ -19,7 +16,7 @@ if __name__ == '__main__':
 Deploy ERC20Template and Factory to a target network. 
 
 Usage: deploy.py NETWORK
-  NETWORK -- one of: {ALLOWED_NETWORKS}
+  NETWORK -- one of: {ALLOWED_NETWORKS_STR}
 
 Notes:
  -'development' means ganache 
@@ -30,41 +27,42 @@ Notes:
     # ****SET INPUT ARGS****
     #got the right number of args?  If not, output help
     num_args = len(sys.argv) - 1
-    num_args_needed = [1]
-    if num_args not in num_args_needed:
+    num_args_needed = 1
+    if num_args != num_args_needed:
         print(help)
         if num_args > 0:
             print("Got %d argument(s), need %s.\n" % (num_args, num_args_needed))
         sys.exit(0)
     
-    #input args: get them
-    network = eval(sys.argv[1])
-
+    #grab args
+    network = sys.argv[1]
     print("Arguments: NETWORK=%s\n" % network)
 
-    #input args: handle corner cases
+    #corner cases
     if network not in ALLOWED_NETWORKS:
-        print(f"Invalid network. Allowed networks: {ALLOWED_NETWORKS}")
+        print(f"Invalid network. Allowed networks: {ALLOWED_NETWORKS_STR}")
         sys.exit(0)
 
     # ****SET ENVT****
-    #grab from env't
+    #grab vars
     opf_private_key = os.getenv('OPF_PRIVATE_KEY')
     community_addr = os.getenv('OCEAN_COMMUNITY_ADDRESS')
 
-    #sanity check envt
+    #corner cases 
     if opf_private_key is None:
         print("Need environmental var OPF_PRIVATE_KEY")
         sys.exit(0)
     if community_addr is None:
         print("Need environmental var OCEAN_COMMUNITY_ADDRESS")
         sys.exit(0)
-    
+
     # ****DEPLOY****
     if not brownie.network.is_connected():
         brownie.network.connect(network)
 
     print("Deploy ERC20Template...")
+    from brownie import *
+    import pdb; pdb.set_trace()
     ERC20_template = ERC20Template.deploy(
         'Template', 'TEMPLATE', opf_account.address,
         community_addr, {'from': alice_account})
