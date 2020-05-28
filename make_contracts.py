b055f1f44dae82e5efa3ab653f81d6926d349d46
trentmc <5305452+trentmc@users.noreply.github.com>
2020-05-28 17:05:17 +0200

    update

--- make_contracts.py
+++ make_contracts.py
@@ -6,14 +6,14 @@ import shutil
 import subprocess
 import sys
 
-print("Check Python version")
-s = subprocess.check_output("python --version", shell=True) #eg "Python 2.7.17"
+print('===Check Python version')
+s = subprocess.check_output('python --version', shell=True) #eg 'Python 2.7.17'
 version = float(re.findall(' \d\.[\d|\d\d]', str(s))[0]) #eg 2.7
 if version < 3.6:
-    print("Need >= Python 3.6")
+    print('===Need >= Python 3.6')
     sys.exit(0)
 
-print("Check virtualenv")
+print('===Check virtualenv')
 if 'myenv' not in str(os.environ.get('VIRTUAL_ENV')):
     print("""
 Virtual environment not running. Have you done these? 
@@ -22,13 +22,21 @@ source myenv/bin/activate
 pip install -r requirements.txt""")
     sys.exit(0)
     
-print('git clone .sol sources')
+print('===Clone/update .sol sources: begin')
 if not os.path.exists('../ocean-contracts'):
+    print('===  Clone ocean-contracts')
     os.system('cd ..; git clone https://github.com/oceanprotocol/ocean-contracts; cd -')
+else:
+    print('===  Update ocean-contracts')
+    os.system('cd ../ocean-contracts; git pull; cd -')
+    
 if not os.path.exists('../openzeppelin-contracts'): #note that we use v0.2.5 
+    print('===  Clone openzeppelin-contracts')
     os.system('cd ..; git clone --branch v2.5.0 https://github.com/OpenZeppelin/openzeppelin-contracts.git; cd -')
+    #don't need to ever update them, since it's an old version
+print('===Clone/update .sol sources: done')
 
-print('populate ./contracts')
+print('===Populate ./contracts')
 if os.path.exists('./contracts'):
     os.system('rm -rf contracts')
 os.mkdir('contracts')
@@ -52,9 +60,9 @@ def inplace_change(filename, old_s, new_s):
         s = s.replace(old_s, new_s)
         f.write(s)
 
-print('flatten imports in .sol files')
+print('===Flatten imports in .sol files')
 import glob
-for f in glob.glob("contracts/*.sol"):
+for f in glob.glob('contracts/*.sol'):
     inplace_change(f, 'openzeppelin-solidity/contracts/', './')
     inplace_change(f, '../../', './')
     inplace_change(f, '../', './')
@@ -66,3 +74,5 @@ for f in glob.glob("contracts/*.sol"):
     inplace_change(f, 'ERC20/', '')
     inplace_change(f, 'interfaces/', '')
     inplace_change(f, 'ownership/', '')
+
+print('===Done!')
