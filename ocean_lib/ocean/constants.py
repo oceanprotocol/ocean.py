bed052c98070896f64ab23aa1742a80552418a9c
trentmc <5305452+trentmc@users.noreply.github.com>
2020-06-29 07:13:51 +0200

    have a means to toggle type-checking

---
+++ ocean_lib/ocean/constants.py
@@ -0,0 +1,18 @@
+# configuration file
+CONF_FILE_PATH = '~/ocean.conf'
+
+#Toggle runtime type-checking
+import configparser, os
+config = configparser.ConfigParser()
+config.read(os.path.expanduser(CONF_FILE_PATH))
+
+TYPECHECK = config['util'].getboolean('typecheck')
+assert TYPECHECK is not None
+
+import enforce
+if not TYPECHECK:
+    # do nothing, just return the original function
+    def noop(f):
+        return f
+    enforce.runtime_validation = noop
+    
