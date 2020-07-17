87835beb4e2dc046e1a159c98659849751e16b5d
ssallam <travis@travis-ci.org>
2020-07-17 15:21:24 +0200

    update version.

--- setup.py
+++ setup.py
@@ -93,6 +93,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/ocean-lib-py',
-    version='0.1.0',
+    version='0.0.0',
     zip_safe=False,
 )
