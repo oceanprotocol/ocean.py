d51fa53285e013ba8d6d65d635c62d0ca87d1989
ssallam <travis@travis-ci.org>
2020-09-01 09:07:28 +0200

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -97,6 +97,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/ocean-lib-py',
-    version='0.2.0',
+    version='0.3.0',
     zip_safe=False,
 )
