12cee5d453011d18d9dda39b2fc8ab26d1036fee
ssallam <travis@travis-ci.org>
2020-09-03 15:07:24 +0200

    add more to ddo tests.

--- tests/models/test_ddo.py
+++ tests/models/test_ddo.py
@@ -10,9 +10,8 @@ from ocean_lib.assets.asset import Asset
 from ocean_lib.config_provider import ConfigProvider
 from ocean_lib.models.ddo import DDOContract
 from ocean_lib.ocean.util import get_contracts_addresses
-from ocean_lib.web3_internal.event_filter import EventFilter
 from ocean_lib.web3_internal.web3_provider import Web3Provider
-from tests.resources.helper_functions import get_resource_path, get_publisher_wallet
+from tests.resources.helper_functions import get_resource_path, get_publisher_wallet, get_consumer_wallet
 
 
 def get_ddo_sample():
@@ -71,19 +70,6 @@ def test_ddo_on_chain():
     )
     assert ddo_registry.verify_tx(txid), f'update ddo failed: txid={txid}'
     logs = ddo_registry.events.DDOUpdated().processReceipt(ddo_registry.get_tx_receipt(txid))
-    if not logs:
-        event = getattr(ddo_registry.events, 'DDOUpdated')
-        # TODO: use the filter on tokenAddress when it is set as indexed in the contract event.
-        filter_params = {}
-        event_filter = EventFilter(
-            'DDOUpdated',
-            event,
-            filter_params,
-            from_block=0,
-            to_block='latest'
-        )
-        logs = event_filter.get_all_entries(max_tries=10)
-
     assert logs, f'no logs found for update ddo tx {txid}'
     log = logs[0]
     assert add_0x_prefix(log.args.did.hex()) == asset.asset_id
@@ -92,7 +78,30 @@ def test_ddo_on_chain():
     assert ddo_text == asset.as_text(), f'ddo text does not match original.'
     _asset = Asset(json_text=ddo_text)
     assert _asset.metadata['main']['name'] == 'updated name for test', f'name does not seem to be updated.'
+    assert ddo_registry.didOwner(asset.asset_id) == wallet.address
+
+    # test update fails from wallet other than the original publisher
+    bob = get_consumer_wallet()
+    try:
+        txid = ddo_registry.update(
+            asset.asset_id,
+            b'',
+            lzma.compress(web3.toBytes(text=asset.as_text())),
+            bob
+        )
+        assert ddo_registry.verify_tx(txid) is False, f'update ddo failed: txid={txid}'
+        logs = ddo_registry.events.DDOUpdated().processReceipt(ddo_registry.get_tx_receipt(txid))
+        assert not logs, f'should be no logs for DDOUpdated, but seems there are some logs: tx {txid}, logs {logs}'
+    except ValueError:
+        print(f'as expected, only owner can update a published ddo.')
 
     # test ddoOwner
+    assert ddo_registry.didOwner(asset.asset_id) == wallet.address, \
+        f'ddo owner does not match the expected publisher address {wallet.address}, ' \
+        f'owner is {ddo_registry.didOwner(asset.asset_id)}'
 
     # test transferOwnership
+    txid = ddo_registry.transferOwnership(asset.asset_id, bob.address, wallet)
+    assert ddo_registry.verify_tx(txid), f'ddo transferOwnership failed: txid={txid}'
+    assert ddo_registry.didOwner(asset.asset_id) == bob.address, f'new owner not matching.'
+
