6a4db57a056eec378a7ab8e628fb978806f8a44d
ssallam <travis@travis-ci.org>
2020-09-15 17:25:15 +0200

    Bump version: 0.3.2 → 0.3.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ copyright = 'ocean-lib-py contributors'
 author = 'ocean-lib-py contributors'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.2'
+release = '0.3.3'
 # The short X.Y version
 release_parts = release.split('.')  # a list
 version = release_parts[0] + '.' + release_parts[1]
--- ocean_lib/__init__.py
+++ ocean_lib/__init__.py
@@ -1,5 +1,5 @@
 __author__ = """OceanProtocol"""
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 
 #  Copyright 2018 Ocean Protocol Foundation
 #  SPDX-License-Identifier: Apache-2.0
