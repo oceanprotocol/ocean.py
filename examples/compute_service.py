3ba07b92c394a30a1adc7e1e682fd370e0dcfeb2
dhruv <dhruv96@gmail.com>
2020-12-30 21:41:48 +0530

    update

--- examples/compute_service.py
+++ examples/compute_service.py
@@ -155,7 +155,7 @@ def main(did, pool_address, order_tx_id=None):
 
     print(f'Requesting compute using asset {asset.did} and pool {pool.address}')
     algo_file = './examples/data/algorithm.py'
-    order_tx_id=
+    #order_tx_id=
 
     job_id, status = run_compute(asset.did, consumer, algo_file, pool.address, order_tx_id)
     print(f'Compute started on asset {asset.did}: job_id={job_id}, status={status}')
