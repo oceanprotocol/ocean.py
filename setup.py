6ae4f7d081968faedb1574c2d76b2762c74c550f
ssallam <travis@travis-ci.org>
2021-02-10 11:11:39 +0100

    Bump version: 0.5.6 → 0.5.7

--- setup.py
+++ setup.py
@@ -102,6 +102,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/ocean.py',
-    version='0.5.6',
+    version='0.5.7',
     zip_safe=False,
 )
