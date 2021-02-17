b611452337433bbe4a0a719c3d93f81b7b53d83c
alexcos20 <alex.coseru@gmail.com>
2021-02-17 05:44:57 -0800

    Bump version: 0.5.8 → 0.5.9

--- ocean_lib/__init__.py
+++ ocean_lib/__init__.py
@@ -1,5 +1,5 @@
 __author__ = """OceanProtocol"""
-__version__ = "__version__ = '0.5.8'"
+__version__ = "__version__ = '0.5.9'"
 
 #  Copyright 2018 Ocean Protocol Foundation
 #  SPDX-License-Identifier: Apache-2.0
