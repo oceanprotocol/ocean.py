c92d3982cd09cb75e919c003a974dfbf43ca5dfd
Calina Cenan <calina.tutunaru@algotech.solutions>
2021-02-23 10:58:11 +0200

    Adds specific None test.

--- tests/ocean/test_market_flow.py
+++ tests/ocean/test_market_flow.py
@@ -18,6 +18,66 @@ from tests.resources.helper_functions import (
 )
 
 
+def test_pay_for_service_none_consumer():
+    pub_wallet = get_publisher_wallet()
+
+    publisher_ocean = get_publisher_ocean_instance()
+    consumer_ocean = get_consumer_ocean_instance()
+
+    # Register Asset
+    asset = get_registered_ddo(publisher_ocean, get_metadata(), pub_wallet)
+    assert isinstance(asset, Asset)
+    assert asset.data_token_address
+
+    consumer_wallet = get_consumer_wallet()
+
+    service = asset.get_service(service_type=ServiceTypes.ASSET_ACCESS)
+    sa = ServiceAgreement.from_json(service.as_dictionary())
+
+    # Mint data tokens and assign to publisher
+    dt = publisher_ocean.get_data_token(asset.data_token_address)
+    mint_tokens_and_wait(dt, pub_wallet.address, pub_wallet)
+
+    ######
+    # Give the consumer some datatokens so they can order the service
+    try:
+        tx_id = dt.transfer_tokens(consumer_wallet.address, 10, pub_wallet)
+        dt.verify_transfer_tx(tx_id, pub_wallet.address, consumer_wallet.address)
+    except (AssertionError, Exception) as e:
+        print(e)
+        raise
+
+    ######
+    # Place order for the download service
+    order_requirements = consumer_ocean.assets.order(
+        asset.did, consumer_wallet.address, sa.index
+    )
+
+    ######
+    # Pay for the service
+    _order_tx_id = consumer_ocean.assets.pay_for_service(
+        order_requirements.amount,
+        order_requirements.data_token_address,
+        asset.did,
+        service.index,
+        "0xF9f2DB837b3db03Be72252fAeD2f6E0b73E428b9",
+        consumer_wallet,
+        None,
+    )
+
+    ######
+    # Download the asset files
+    asset_folder = consumer_ocean.assets.download(
+        asset.did,
+        sa.index,
+        consumer_wallet,
+        _order_tx_id,
+        consumer_ocean.config.downloads_path,
+    )
+
+    assert len(os.listdir(asset_folder)) >= 1
+
+
 def test_market_flow():
     pub_wallet = get_publisher_wallet()
 
