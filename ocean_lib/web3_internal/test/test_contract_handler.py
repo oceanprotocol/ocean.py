58c93223a2828320f8bf9ab51698a6db67a57754
trentmc <5305452+trentmc@users.noreply.github.com>
2021-02-24 11:19:59 +0100

    test base cases

--- ocean_lib/web3_internal/test/test_contract_handler.py
+++ ocean_lib/web3_internal/test/test_contract_handler.py
@@ -1,12 +1,17 @@
 #  Copyright 2021 Ocean Protocol Foundation
 #  SPDX-License-Identifier: Apache-2.0
 
-from ocean_lib.config_provider import ConfigProvider
 from ocean_lib.web3_internal.contract_handler import ContractHandler
 
 _NETWORK = "ganache"
 
-def test1():
-    config = ConfigProvider.get_config()
-    contracts_addresses = ContractHandler.get_contracts_addresses(
-        _NETWORK, config.address_file)
+def test_empty_address_path():
+    assert ContractHandler.get_contracts_addresses(
+        _NETWORK, address_file=None) is None
+    
+def test_nonexistent_address_path():
+    assert ContractHandler.get_contracts_addresses(
+        _NETWORK, address_file="not a path") is None
+    
+    assert ContractHandler.get_contracts_addresses(
+        _NETWORK, address_file="/bin/foo/bar/tralala") is None
