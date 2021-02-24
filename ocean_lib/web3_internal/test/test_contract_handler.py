6a2cdf8387d4eb4ee57115ca39be75b0ebf99c30
trentmc <5305452+trentmc@users.noreply.github.com>
2021-02-24 12:09:02 +0100

    happy-path test

--- ocean_lib/web3_internal/test/test_contract_handler.py
+++ ocean_lib/web3_internal/test/test_contract_handler.py
@@ -5,13 +5,30 @@ from ocean_lib.web3_internal.contract_handler import ContractHandler
 
 _NETWORK = "ganache"
 
-def test_get_contracts_addresses__empty_address_path():
+def test_get_contracts_addresses__empty_path():
     assert ContractHandler.get_contracts_addresses(
         _NETWORK, address_file=None) is None
     
-def test_get_contracts_addresses__nonexistent_address_path():
-    assert ContractHandler.get_contracts_addresses(
-        _NETWORK, address_file="not a path") is None
-    
-    assert ContractHandler.get_contracts_addresses(
+def test_get_contracts_addresses__nonexistent_path():    
+    ContractHandler.get_contracts_addresses(
         _NETWORK, address_file="/bin/foo/bar/tralala") is None
+
+def test_get_contracts_addresses__real_path(tmp_path):
+    #tmp_path is a pathlib.Path object
+    #More info: https://docs.pytest.org/en/stable/tmpdir.html
+
+    file_content = '{"%s" : "my address values"}' % _NETWORK
+    
+    #create test file, fill it
+    d = tmp_path / "sub"
+    d.mkdir()
+    address_file = d / "address.json"
+    address_file.write_text(file_content)
+    assert address_file.read_text() == file_content
+    assert len(list(tmp_path.iterdir())) == 1
+
+    #the main test
+    network_addresses = ContractHandler.get_contracts_addresses(
+        _NETWORK, address_file=address_file)
+    assert network_addresses == "my address values"
+
