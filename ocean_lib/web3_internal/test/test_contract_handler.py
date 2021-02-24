ce8fa5ca90b351bcb9aab634c114dc308dff79f1
trentmc <5305452+trentmc@users.noreply.github.com>
2021-02-24 12:19:38 +0100

    streamline

--- ocean_lib/web3_internal/test/test_contract_handler.py
+++ ocean_lib/web3_internal/test/test_contract_handler.py
@@ -13,19 +13,33 @@ def test_get_contracts_addresses__nonexistent_path():
     ContractHandler.get_contracts_addresses(
         _NETWORK, address_file="/bin/foo/bar/tralala") is None
 
-def test_get_contracts_addresses__real_path(tmp_path):
-    #tmp_path is a pathlib.Path object
-    #More info: https://docs.pytest.org/en/stable/tmpdir.html
+def test_get_contracts_addresses__custom_network(tmp_path):
+    #tmp_path:pathlib.Path is special pytest feature
 
     #create & fill test file
-    d = tmp_path / "sub"
+    d = tmp_path / "subdir"
     d.mkdir()
     address_file = d / "address.json"
-    file_content = '{"%s" : "myvals"}' % _NETWORK
-    address_file.write_text(file_content)
+    address_file.write_text('{"my_custom_network" : "myvals"}')
 
     #the main test
     network_addresses = ContractHandler.get_contracts_addresses(
-        _NETWORK, address_file=address_file)
+        network="my_custom_network", address_file=address_file)
+    assert network_addresses == "myvals"
+
+def test_get_contracts_addresses__use_network_alias(tmp_path):
+    #tmp_path:pathlib.Path is special pytest feature
+
+    assert ContractHandler.network_alias == {"ganache" : "development"}
+
+    #create & fill test file
+    d = tmp_path / "subdir"
+    d.mkdir()
+    address_file = d / "address.json"
+    address_file.write_text('{"development" : "myvals"}') #not "ganache"
+
+    #the main test
+    network_addresses = ContractHandler.get_contracts_addresses(
+        network="ganache", address_file=address_file)
     assert network_addresses == "myvals"
 
