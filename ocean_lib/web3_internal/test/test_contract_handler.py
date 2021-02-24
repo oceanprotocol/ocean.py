322515fdc32fadafc661745bf9eb5a63bf235705
trentmc <5305452+trentmc@users.noreply.github.com>
2021-02-24 11:16:06 +0100

    basic test for config_provider

--- ocean_lib/web3_internal/test/test_contract_handler.py
+++ ocean_lib/web3_internal/test/test_contract_handler.py
@@ -1,5 +1,12 @@
 #  Copyright 2021 Ocean Protocol Foundation
 #  SPDX-License-Identifier: Apache-2.0
 
+from ocean_lib.config_provider import ConfigProvider
+from ocean_lib.web3_internal.contract_handler import ContractHandler
+
+_NETWORK = "ganache"
+
 def test1():
-    pass
+    config = ConfigProvider.get_config()
+    contracts_addresses = ContractHandler.get_contracts_addresses(
+        _NETWORK, config.address_file)
