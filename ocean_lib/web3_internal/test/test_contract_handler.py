9f961dbdb84b36951459d9be97f74512e5f69f27
trentmc <5305452+trentmc@users.noreply.github.com>
2021-02-24 12:11:12 +0100

    streamline happy-path test

--- ocean_lib/web3_internal/test/test_contract_handler.py
+++ ocean_lib/web3_internal/test/test_contract_handler.py
@@ -17,15 +17,12 @@ def test_get_contracts_addresses__real_path(tmp_path):
     #tmp_path is a pathlib.Path object
     #More info: https://docs.pytest.org/en/stable/tmpdir.html
 
-    file_content = '{"%s" : "my address values"}' % _NETWORK
-    
-    #create test file, fill it
+    #create & fill test file
     d = tmp_path / "sub"
     d.mkdir()
     address_file = d / "address.json"
+    file_content = '{"%s" : "my address values"}' % _NETWORK
     address_file.write_text(file_content)
-    assert address_file.read_text() == file_content
-    assert len(list(tmp_path.iterdir())) == 1
 
     #the main test
     network_addresses = ContractHandler.get_contracts_addresses(
