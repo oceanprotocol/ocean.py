43892adc28a8377c630ce0594e171eb62953ae8c
trentmc <5305452+trentmc@users.noreply.github.com>
2021-02-27 08:49:05 +0100

    basic setup for testing ContractBase

--- ocean_lib/web3_internal/test/test_contract_base.py
+++ ocean_lib/web3_internal/test/test_contract_base.py
@@ -1,8 +1,22 @@
 #  Copyright 2021 Ocean Protocol Foundation
 #  SPDX-License-Identifier: Apache-2.0
 
-# from ocean_lib.web3_internal.contract_base import ContractBase
+from ocean_lib.web3_internal.contract_base import ContractBase
 
 
-class test1:
-    pass
+def test1(network, alice_wallet, alice_address, OCEAN_address):
+    class MyBToken(ContractBase):
+        CONTRACT_NAME = "BToken"
+
+        # super-simple functionality, because our main point here is to
+        # test ContractBase itself, not a child class.
+        def symbol(self) -> str:
+            return self.contract_concise.symbol()
+
+    # does the super-simple functionality work?
+    token = MyBToken(OCEAN_address)
+    assert token.symbol() == "OCEAN"
+
+    # now test ContractBase itself...
+
+    # FIXME
