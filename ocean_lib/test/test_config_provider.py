9b07027aebee50493409f32de175edb7b996dd6d
Trent McConaghy <5305452+trentmc@users.noreply.github.com>
2021-03-03 09:46:47 +0200

    issue95 deprecate artifacts (#171)
    Fixes #95.
    
    Changes proposed in this PR:
    - delete `artifacts/` dir, and point config.ini to barge's artifacts. That's it.
    - Intro'd unit tests for the modules using this. This is the start of a more scalable location for unit tests: in sub-directories of the modules themselves, called `test/`. In this case it's `web3_internal/test/`.
    - Updated setting of setup.py's `packages` variable to ignore new test directories, and made it simpler too.
    
    Co-authored-by: Samer <36411297+ssallam@users.noreply.github.com>

---
+++ ocean_lib/test/test_config_provider.py
@@ -0,0 +1,11 @@
+#  Copyright 2021 Ocean Protocol Foundation
+#  SPDX-License-Identifier: Apache-2.0
+
+from ocean_lib.config_provider import ConfigProvider
+
+
+def test1():
+    config = "foo config"
+    ConfigProvider.set_config(config)
+
+    assert ConfigProvider.get_config() == "foo config"
