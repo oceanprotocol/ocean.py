81627ea89fc5372e23a4913033704d38be7f068f
Calina Cenan <calina.tutunaru@algotech.solutions>
2021-03-03 11:26:13 +0200

    Removes unused imports in setup.py.

--- setup.py
+++ setup.py
@@ -10,10 +10,7 @@
 #  Copyright 2018 Ocean Protocol Foundation
 #  SPDX-License-Identifier: Apache-2.0
 
-import os
-from os.path import join
-
-from setuptools import setup, find_namespace_packages
+from setuptools import find_namespace_packages, setup
 
 with open("README.md", encoding="utf8") as readme_file:
     readme = readme_file.read()
