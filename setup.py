4df90e6bb4bb15495959ff9d094e9b3599ac7a0b
trentmc <5305452+trentmc@users.noreply.github.com>
2021-03-05 06:59:48 +0100

    fix #194

--- setup.py
+++ setup.py
@@ -99,6 +99,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/oceanprotocol/ocean.py",
-    version="0.5.11",
+    version='0.5.11', #bumpversion.sh needs single-quotes
     zip_safe=False,
 )
