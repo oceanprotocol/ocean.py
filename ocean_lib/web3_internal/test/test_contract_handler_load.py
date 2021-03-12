db01e32d599b000e9b044ca9b7737455bbe5e77d
trentmc <5305452+trentmc@users.noreply.github.com>
2021-03-12 09:25:57 +0100

    I can now reproduce the failure. To do so, I needed to be able to disable class attributes set in setup_all() and delete a copule envvars in the unit test. For the former, I created two new tests on disabling setup_all()

--- ocean_lib/web3_internal/test/test_contract_handler_load.py
+++ ocean_lib/web3_internal/test/test_contract_handler_load.py
@@ -4,7 +4,10 @@
 #
 
 import pytest
+from ocean_lib.config_provider import ConfigProvider
 from ocean_lib.web3_internal.contract_handler import ContractHandler
+from ocean_lib.web3_internal.web3_provider import Web3Provider
+from ocean_lib.web3_internal.web3helper import Web3Helper
 from web3.exceptions import InvalidAddress
 
 
@@ -50,4 +53,49 @@ def test_load__name_and_address(network, example_config):
     contract = ContractHandler._load("DTFactory", target_address)
 
     assert ContractHandler._contracts[test_tuple] == contract
+
+@pytest.mark.nosetup_all 
+def test_disable_setup_all_1(monkeypatch):
+    """Test that conftest::setup_all() was *not* called due to decorator"""
+    c = ConfigProvider()
+    assert ConfigProvider._config is None
+    assert Web3Provider._web3 is None
+    assert ContractHandler._contracts == dict()
+    assert ContractHandler.artifacts_path is None
+
+def test_disable_setup_all_2(monkeypatch):
+    """Test that setup_all() *was* called, since no decorator to disable"""
+    assert ConfigProvider._config is not None
+    assert Web3Provider._web3 is not None
+    assert ContractHandler._contracts != dict()
+    assert ContractHandler.artifacts_path is not None
+
+@pytest.mark.nosetup_all #disable call to conftest.py::setup_all()
+def test_issue185(monkeypatch):
+    #disable envvars that imports may have brought in
+    import os
+            
+    if os.getenv('CONFIG_FILE'):
+        monkeypatch.delenv('CONFIG_FILE')
+    assert os.getenv('CONFIG_FILE') is None, "can't have CONFIG_FILE envvar set"
+
+
+    if os.getenv('ARTIFACTS_PATH'):
+        monkeypatch.delenv('ARTIFACTS_PATH')
+    assert os.getenv('ARTIFACTS_PATH') is None, "can't have ARTIFACTS_PATH envvar set"
+    
+    #rest of test
+    from ocean_lib.ocean.ocean import Ocean
+    from ocean_lib.web3_internal.wallet import Wallet
+    
+    private_key = os.getenv('TEST_PRIVATE_KEY1')
+    config = {'network': os.getenv('NETWORK_URL')}
+    ocean = Ocean(config)
+
+    print("create wallet: begin")
+    wallet = Wallet(ocean.web3, private_key=private_key)
+    print(f"create wallet: done. Its address is {wallet.address}")
+
+    print("create datatoken: begin.")
+    datatoken = ocean.create_data_token("Dataset name", "dtsymbol", from_wallet=wallet) 
 
