b581a61006ae6e057d8b0699caab014165abefe0
Calina Cenan <calina@oceanprotocol.com>
2021-03-16 19:44:09 +0200

    Core review changes.

--- ocean_lib/models/test/test_datatoken.py
+++ ocean_lib/models/test/test_datatoken.py
@@ -35,7 +35,21 @@ def test_ERC20(alice_ocean, alice_wallet, alice_address, bob_wallet, bob_address
     assert from_base_18(token.balanceOf(alice_address)) == 98.0
     assert from_base_18(token.balanceOf(bob_address)) == 2.0
 
-    # test status functions
+    # assert transfers were successful
+    block = alice_ocean.web3.eth.blockNumber
+    all_transfers = token.get_all_transfers_from_events(block - 1, block + 1, chunk=1)
+    assert len(all_transfers[0]) == 2
+
+
+def test_status_functions(alice_ocean, alice_wallet, alice_address):
+    """Tests various status functions of the DataToken class."""
+    token = alice_ocean.create_data_token(
+        "DataToken1", "DT1", from_wallet=alice_wallet, blob="foo_blob"
+    )
+
+    token.mint(alice_address, to_base_18(100.0), from_wallet=alice_wallet)
+
+    assert from_base_18(token.balanceOf(alice_address)) == 100.0
     assert token.totalSupply() == 100_000_000_000_000_000_000
     assert token.cap() == 1_000_000_000_000_000_000_000
     assert token.datatoken_name() == "DataToken1"
@@ -51,12 +65,18 @@ def test_ERC20(alice_ocean, alice_wallet, alice_address, bob_wallet, bob_address
     with pytest.raises(ValueError):
         token.get_event_signature("not a registered event")
 
-    all_transfers = token.get_all_transfers_from_events(block - 1, block + 1, chunk=1)
-    assert len(all_transfers[0]) == 2
-
 
 def test_blob(alice_ocean, alice_wallet):
-    """Tests DataToken creation with blob."""
+    """Tests DataToken creation with a string blob."""
+    token = alice_ocean.create_data_token(
+        "DataToken1", "DT1", alice_wallet, blob="foo_bar"
+    )
+    assert token.blob() == "foo_bar"
+    assert token.get_simple_url() is None
+
+
+def test_blob_json(alice_ocean, alice_wallet):
+    """Tests DataToken creation with a json blob."""
     blob_dict = {"t": 0, "url": "http://tblob/", "foo": "bar"}
     token = alice_ocean.create_data_token(
         "DataToken1", "DT1", alice_wallet, blob=json.dumps(blob_dict)
@@ -69,13 +89,6 @@ def test_blob(alice_ocean, alice_wallet):
         "DataToken1", "DT1", alice_wallet, blob=json.dumps(blob_dict)
     )
     assert token.get_metadata_url() == "http://tblob/"
-
-    blob_string = "foo_bar"
-    token = alice_ocean.create_data_token(
-        "DataToken1", "DT1", alice_wallet, blob=blob_string
-    )
-    assert token.blob() == blob_string
-    assert token.get_simple_url() is None
 
 
 def test_static_methods():
