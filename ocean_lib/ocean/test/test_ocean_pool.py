0856c93e4d090952e1c89a20601dac4f3b2622c3
Calina Cenan <calina@oceanprotocol.com>
2021-03-19 11:04:15 +0200

    Adds test for removing 0 liquidity in ocean pool.

--- ocean_lib/ocean/test/test_ocean_pool.py
+++ ocean_lib/ocean/test/test_ocean_pool.py
@@ -4,6 +4,7 @@
 #
 from ocean_lib.ocean.util import get_ocean_token_address
 from ocean_lib.web3_internal.web3helper import Web3Helper
+from tests.resources.helper_functions import get_publisher_wallet
 
 
 def test_get_OCEAN_address(publisher_ocean_instance):
@@ -11,4 +12,20 @@ def test_get_OCEAN_address(publisher_ocean_instance):
     network = Web3Helper.get_network_name()
     assert publisher_ocean_instance.pool.get_OCEAN_address() == get_ocean_token_address(
         network
+    )
+
+
+def test_add_remove_zero_liquidity(publisher_ocean_instance):
+    """Tests that adding or removing zero liquidity has no effect."""
+    assert (
+        publisher_ocean_instance.pool._add_liquidity(
+            "addr", "an_addr", 0, get_publisher_wallet()
+        )
+        == ""
+    )
+    assert (
+        publisher_ocean_instance.pool._remove_liquidity(
+            "addr", "an_addr", 0, 1, get_publisher_wallet()
+        )
+        == ""
     )
