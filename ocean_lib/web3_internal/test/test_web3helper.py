b5cb6896e8cb374e9be56d8b03be725ea83cd398
Calina Cenan <calina@oceanprotocol.com>
2021-03-24 17:46:36 +0200

    Add coverage for web3helper.

---
+++ ocean_lib/web3_internal/test/test_web3helper.py
@@ -0,0 +1,13 @@
+#
+# Copyright 2021 Ocean Protocol Foundation
+# SPDX-License-Identifier: Apache-2.0
+#
+from ocean_lib.web3_internal.web3helper import Web3Helper
+
+
+def test_send_ether(alice_wallet, bob_address):
+    assert Web3Helper.send_ether(alice_wallet, bob_address, 1)
+
+
+def test_cancel_or_replace_transaction(alice_wallet):
+    assert Web3Helper.cancel_or_replace_transaction(alice_wallet, None)
