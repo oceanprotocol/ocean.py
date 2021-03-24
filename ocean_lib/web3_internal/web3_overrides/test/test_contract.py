21318faf7558ca5acbcd005b705f3b469795af68
Calina Cenan <calina@oceanprotocol.com>
2021-03-24 14:06:22 +0200

    Add some coveerage to custom contracts.

--- ocean_lib/web3_internal/web3_overrides/test/test_contract.py
+++ ocean_lib/web3_internal/web3_overrides/test/test_contract.py
@@ -3,21 +3,20 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 import pytest
-from ocean_lib.models.test.conftest import dtfactory_address
+from ocean_lib.models.test.conftest import *  # noqa
 from ocean_lib.ocean.util import to_base_18
 from ocean_lib.web3_internal.test.test_contract_base import MyFactory
 from ocean_lib.web3_internal.web3_overrides.contract import CustomContractFunction
 
 
-def test_main():
+def test_main(dtfactory_address):
     factory = MyFactory(dtfactory_address)
     # factory.createToken("foo_blob", "DT1", "DT1", to_base_18(1000), alice_wallet)
     fn_args = ("foo_blob", "DT1", "DT1", to_base_18(1000))
     contract_fn = getattr(factory.contract.functions, "createToken")(*fn_args)
     custom_contract = CustomContractFunction(contract_fn)
 
-    # TODO: None transaction doesn't actually work.
-    # assert custom_contract.transact() == None
+    # TODO: transaction with passphrase and account key, need to mock web3
 
     with pytest.raises(ValueError):
         custom_contract.transact({"data": "test"})
@@ -25,4 +24,4 @@ def test_main():
     with pytest.raises(ValueError):
         custom_contract = CustomContractFunction(contract_fn)
         custom_contract._contract_function.address = None
-        custom_contract.transact()
+        custom_contract.transact({})
