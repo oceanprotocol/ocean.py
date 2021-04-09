12e8a741778ed8e617547d6f21efee4d78bb0622
mariacarmina <mariacarmina.cretu@gmail.com>
2021-04-09 19:14:45 +0300

    93% code coverage for utils.py from assets folder. Updated test private keys from tox.ini to match pytest.

--- ocean_lib/assets/test/test_utils.py
+++ ocean_lib/assets/test/test_utils.py
@@ -13,6 +13,7 @@ from tests.resources.helper_functions import get_publisher_wallet
 from ocean_lib.assets.utils import (
     create_publisher_trusted_algorithms,
     add_publisher_trusted_algorithm,
+    remove_publisher_trusted_algorithm,
 )
 
 
@@ -67,3 +68,31 @@ def test_add_trusted_algorithm_no_compute_service(publisher_ocean_instance, meta
         add_publisher_trusted_algorithm(
             ddo.did, algorithm_ddo.did, publisher_ocean_instance.config.aquarius_url
         )
+
+
+def test_remove_trusted_algorithm(publisher_ocean_instance):
+    """Tests removing trusted algorithms in the DDO metadata."""
+    publisher = get_publisher_wallet()
+
+    algorithm_ddo = get_registered_algorithm_ddo(publisher_ocean_instance, publisher)
+    _ = publisher_ocean_instance.assets.resolve(algorithm_ddo.did)
+    assert algorithm_ddo is not None
+
+    ddo = get_registered_ddo_with_compute_service(
+        publisher_ocean_instance,
+        publisher,
+        trusted_algorithms=[algorithm_ddo.did],
+    )
+    _ = publisher_ocean_instance.assets.resolve(ddo.did)
+    assert ddo is not None
+
+    publisher_trusted_algorithms = create_publisher_trusted_algorithms(
+        [algorithm_ddo.did], publisher_ocean_instance.config.aquarius_url
+    )
+
+    new_publisher_trusted_algorithms = remove_publisher_trusted_algorithm(
+        ddo.did, algorithm_ddo.did, publisher_ocean_instance.config.aquarius_url
+    )
+
+    assert new_publisher_trusted_algorithms is not None
+    assert len(new_publisher_trusted_algorithms) <= len(publisher_trusted_algorithms)
