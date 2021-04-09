7d6a9447fd2fcec8efe763373830e6bda207535c
trentmc <5305452+trentmc@users.noreply.github.com>
2021-04-09 07:53:48 +0200

    fix #270

--- ocean_lib/enforce_typing_shim.py
+++ ocean_lib/enforce_typing_shim.py
@@ -5,7 +5,7 @@
 import os
 
 from enforce_typing import enforce_types
-from ocean_lib.config import Config
+from ocean_lib.config import Config, config_defaults, NAME_TYPECHECK
 from ocean_lib.config_provider import ConfigProvider
 from ocean_lib.ocean.env_constants import ENV_CONFIG_FILE
 
@@ -15,6 +15,15 @@ def setup_enforce_typing_shim():
 
 
 def enforce_types_shim(func):
-    if not ConfigProvider.get_config()["util"].getboolean("typecheck"):
+    try:
+        c = ConfigProvider.get_config()
+        typecheck = c["util"].getboolean("typecheck")
+        
+    except AssertionError: #handle if ConfigProvider.set_config() not done yet
+        typecheck = config_defaults["util"][NAME_TYPECHECK]
+
+    if typecheck:
+        return enforce_types(func)
+    else:
         return func
-    return enforce_types(func)
+
