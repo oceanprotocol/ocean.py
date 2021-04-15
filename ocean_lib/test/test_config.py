4ae324b80957b6a7503af0f5ff7ffd65908caa56
mariacarmina <mariacarmina.cretu@gmail.com>
2021-04-15 18:41:07 +0300

    Added docstring.

--- ocean_lib/test/test_config.py
+++ ocean_lib/test/test_config.py
@@ -7,6 +7,7 @@ from ocean_lib.config import Config
 
 
 def test_metadata_cache_uri_property():
+    """Tests if the URL of the aquarius matches 'metadata_cache_uri' property."""
     config = Config()
     metadata_cache_uri = config.metadata_cache_uri
     assert metadata_cache_uri
