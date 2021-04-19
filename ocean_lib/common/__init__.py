6f1b446b4f9893ec21e977dc7d238ca9e3172fed
Calina Cenan <calina@oceanprotocol.com>
2021-04-19 12:23:55 +0300

    Remove old version from ocean_utils.

--- ocean_lib/common/__init__.py
+++ ocean_lib/common/__init__.py
@@ -2,7 +2,3 @@
 # Copyright 2021 Ocean Protocol Foundation
 # SPDX-License-Identifier: Apache-2.0
 #
-__version__ = "0.4.2"
-
-#  Copyright 2018 Ocean Protocol Foundation
-#  SPDX-License-Identifier: Apache-2.0
