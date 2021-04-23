4f677b9069789a146cffdc604b4d817189c24bcc
David Hunt-Mateo <david@oceanprotocol.com>
2021-04-23 17:25:13 -0400

    Fix incorrect return type hints for DataToken methods

--- ocean_lib/models/data_token.py
+++ ocean_lib/models/data_token.py
@@ -95,7 +95,7 @@ class DataToken(ContractBase):
     def blob(self) -> str:
         return self.contract_concise.blob()
 
-    def cap(self) -> str:
+    def cap(self) -> int:
         return self.contract_concise.cap()
 
     def isMinter(self, address: str) -> bool:
@@ -112,7 +112,7 @@ class DataToken(ContractBase):
 
     # ============================================================
     # reflect required ERC20 standard functions
-    def totalSupply(self) -> str:
+    def totalSupply(self) -> int:
         return self.contract_concise.totalSupply()
 
     def balanceOf(self, account: str) -> int:
@@ -142,7 +142,7 @@ class DataToken(ContractBase):
     def symbol(self) -> str:
         return self.contract_concise.symbol()
 
-    def decimals(self) -> str:
+    def decimals(self) -> int:
         return self.contract_concise.decimals()
 
     # ============================================================
