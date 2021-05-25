dbffa4448644af15952f5bb002de4ceecd8b4f22
Calina Cenan <calina@oceanprotocol.com>
2021-05-25 12:27:34 +0300

    Improvements on status.

--- ocean_lib/models/dispenser.py
+++ ocean_lib/models/dispenser.py
@@ -12,11 +12,8 @@ from ocean_lib.web3_internal.wallet import Wallet
 class DispenserContract(ContractBase):
     CONTRACT_NAME = "Dispenser"
 
-    def status(self, dt_address: str) -> list:
-        return self.contract_concise.status(dt_address)
-
-    def status_dict(self, dt_address: str) -> dict:
-        status_as_list = self.status(dt_address)
+    def status(self, dt_address: str) -> dict:
+        status_as_list = self.contract_concise.status(dt_address)
         return {
             "active": status_as_list[0],
             "owner": status_as_list[1],
@@ -26,6 +23,27 @@ class DispenserContract(ContractBase):
             "maxBalance": status_as_list[5],
             "balance": status_as_list[6],
         }
+
+    def is_active(self, dt_address: str) -> bool:
+        return self.status(dt_address).get("active", False)
+
+    def owner(self, dt_address: str) -> str:
+        return self.status(dt_address).get("owner", None)
+
+    def is_minter_approved(self, dt_address: str) -> bool:
+        return self.status(dt_address).get("minterApproved", False)
+
+    def is_true_minter(self, dt_address: str) -> bool:
+        return self.status(dt_address).get("isTrueMinter", False)
+
+    def max_tokens(self, dt_address: str) -> int:
+        return self.status(dt_address).get("maxTokens", 0)
+
+    def max_balance(self, dt_address: str) -> int:
+        return self.status(dt_address).get("maxBalance", 0)
+
+    def balance(self, dt_address: str) -> int:
+        return self.status(dt_address).get("balance", 0)
 
     def activate(
         self, dt_address: str, max_tokens: int, max_balance: int, from_wallet: Wallet
@@ -52,3 +70,5 @@ class DispenserContract(ContractBase):
 
     def owner_withdraw(self, dt_address: str, from_wallet: Wallet):
         return self.send_transaction("ownerWithdraw", (dt_address,), from_wallet)
+
+    # TODO: is dispensable
--- ocean_lib/models/test/test_dispenser.py
+++ ocean_lib/models/test/test_dispenser.py
@@ -11,27 +11,27 @@ def test_dispenser_activation(
     contracts_addresses, alice_ocean, alice_wallet, bob_wallet
 ):
     dispenser_address = contracts_addresses["Dispenser"]
-    contract = DispenserContract(dispenser_address)
+    dispenser = DispenserContract(dispenser_address)
     token = alice_ocean.create_data_token(
         "DataToken1", "DT1", from_wallet=alice_wallet, blob="foo_blob"
     )
-    res = contract.status_dict(token.address)
-    assert res["active"] is False
-    assert res["owner"] is None
-    assert res["minterApproved"] is False
-    assert res["isTrueMinter"] is False
-    assert res["maxTokens"] == 0
-    assert res["maxBalance"] == 0
-    assert res["balance"] == 0
+    assert dispenser.is_active(token.address) is False
+    assert dispenser.owner(token.address) is None
+    assert dispenser.is_minter_approved(token.address) is False
+    assert dispenser.is_true_minter(token.address) is False
+    assert dispenser.max_tokens(token.address) == 0
+    assert dispenser.max_balance(token.address) == 0
+    assert dispenser.balance(token.address) == 0
 
-    contract.activate(token.address, 100, 100, alice_wallet)
-    res = contract.status_dict(token.address)
-    assert res["active"]
-    assert res["owner"] == alice_wallet.address
+    dispenser.activate(token.address, 100, 100, alice_wallet)
+    assert dispenser.is_active(token.address)
+    assert dispenser.owner(token.address) == alice_wallet.address
 
     with pytest.raises(ValueError):
-        contract.deactivate(token.address, bob_wallet)
-    contract.deactivate(token.address, alice_wallet)
+        # try to deactivate a different wallet
+        dispenser.deactivate(token.address, bob_wallet)
+
+    assert dispenser.deactivate(token.address, alice_wallet)
 
 
 def test_dispenser_minting(contracts_addresses, alice_ocean, alice_wallet, bob_wallet):
@@ -43,8 +43,7 @@ def test_dispenser_minting(contracts_addresses, alice_ocean, alice_wallet, bob_w
     dispenser.activate(token.address, 1000, 1000, alice_wallet)
 
     dispenser.make_minter(token.address, alice_wallet)
-    res = dispenser.status_dict(token.address)
-    assert res["minterApproved"]
+    assert dispenser.is_minter_approved(token.address)
     dispenser.dispense(token.address, 1, alice_wallet)
     dispenser.dispense(token.address, 1, bob_wallet)
 
