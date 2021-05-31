26d51230b46d46fcb44bc26a15247ec42df9454c
Calina Cenan <calina@cenan.net>
2021-05-31 07:31:43 +0000

    Adapt dispenser and solve conflicts.

--- ocean_lib/models/dispenser.py
+++ ocean_lib/models/dispenser.py
@@ -2,13 +2,13 @@
 # Copyright 2021 Ocean Protocol Foundation
 # SPDX-License-Identifier: Apache-2.0
 #
-from ocean_lib.enforce_typing_shim import enforce_types_shim
+from enforce_typing import enforce_types
 from ocean_lib.models.data_token import DataToken
 from ocean_lib.web3_internal.contract_base import ContractBase
 from ocean_lib.web3_internal.wallet import Wallet
 
 
-@enforce_types_shim
+@enforce_types
 class DispenserContract(ContractBase):
     CONTRACT_NAME = "Dispenser"
 
