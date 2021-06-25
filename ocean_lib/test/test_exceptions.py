4e6049b330fa8baacd920c266c2d370fba9b82b3
Călina Cenan <calina@kaolin.tech>
2021-06-25 09:10:38 +0300

    Fixes AquariusError exception test when run in isolation. (#355)

--- ocean_lib/test/test_exceptions.py
+++ ocean_lib/test/test_exceptions.py
@@ -10,7 +10,7 @@ from eth_utils import add_0x_prefix
 from ocean_lib.common.ddo.ddo import DDO
 from ocean_lib.exceptions import InsufficientBalance, ContractNotFound, AquariusError
 from ocean_lib.web3_internal.constants import ZERO_ADDRESS
-from tests.resources.ddo_helpers import get_resource_path
+from tests.resources.ddo_helpers import get_resource_path, wait_for_ddo
 from tests.resources.helper_functions import get_publisher_wallet
 
 
@@ -46,7 +46,16 @@ def test_AquariusError(publisher_ocean_instance, metadata):
     metadata_copy = metadata.copy()
     publisher = get_publisher_wallet()
 
-    did = publisher_ocean_instance.assets._get_aquarius().list_assets()[0]
+    all_assets = publisher_ocean_instance.assets._get_aquarius().list_assets()
+
+    if not all_assets:
+        # the test is run in isolation, docker has been pruned,
+        # or Aqua is empty for some other reason
+        ddo = publisher_ocean_instance.assets.create(metadata_copy, publisher)
+        wait_for_ddo(publisher_ocean_instance, ddo.did)
+        all_assets = publisher_ocean_instance.assets._get_aquarius().list_assets()
+
+    did = all_assets[0]
     token_address = add_0x_prefix(did[7:])
 
     with pytest.raises(AquariusError):
