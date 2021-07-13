6dfb1f60968ab0347a912c2e5c3b4fa5a41c758f
Calina Cenan <calina@cenan.net>
2021-07-13 08:06:35 +0000

    Remove more unused functions.

--- ocean_lib/models/test/test_balancer_simple_for_users_flow.py
+++ ocean_lib/models/test/test_balancer_simple_for_users_flow.py
@@ -146,6 +146,3 @@ def test_ocean_balancer_helpers(
 
     assert BPool(alice_ocean.web3, pool_address).address == pool_address
     assert BPool(alice_ocean.web3, pool_address).address == pool_address
-
-    DT_price = bob_ocean.pool.get_token_price(pool_address)
-    assert DT_price > 0.0
