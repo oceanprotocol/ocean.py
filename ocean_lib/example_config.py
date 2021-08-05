0fc8d9b3eb6ce8f9726b43a808020ea2097b510f
Călina Cenan <calina@kaolin.tech>
2021-08-05 11:41:15 +0300

    Simplifies logic for network url. (#421)

--- ocean_lib/example_config.py
+++ ocean_lib/example_config.py
@@ -4,12 +4,10 @@
 #
 
 import logging
-import os
 from typing import Dict, Optional
 
 from enforce_typing import enforce_types
 from ocean_lib.config import Config
-from ocean_lib.ocean.util import get_infura_id, get_infura_url
 
 logging.basicConfig(level=logging.INFO)
 
@@ -17,14 +15,7 @@ logging.basicConfig(level=logging.INFO)
 @enforce_types
 class ExampleConfig:
     @staticmethod
-    def get_config_net() -> str:
-        """
-        :return: value of environment variable `TEST_NET` or default `ganache`
-        """
-        return os.environ.get("TEST_NET", "ganache")
-
-    @staticmethod
-    def get_base_config() -> Dict[str, Dict[str, str]]:
+    def _get_config() -> Dict[str, Dict[str, str]]:
         """
         :return: dict
         """
@@ -39,44 +30,21 @@ class ExampleConfig:
         }
 
     @staticmethod
-    def get_network_config(network_name: str) -> Dict[str, Dict[str, str]]:
-        """
-        :return: dict
-        """
-        config = ExampleConfig.get_base_config()
-        config["eth-network"]["network"] = get_infura_url(get_infura_id(), network_name)
-        return config
-
-    @staticmethod
-    def _get_config(
-        local_node: bool = True, net_name: Optional[str] = None
-    ) -> Dict[str, Dict[str, str]]:
-        """
-        :return: dict
-        """
-        if local_node:
-            return ExampleConfig.get_base_config()
-
-        return ExampleConfig.get_network_config(net_name)
-
-    @staticmethod
     def get_config_dict(
         network_name: Optional[str] = None,
     ) -> Dict[str, Dict[str, str]]:
         """
         :return: dict
         """
-        test_net = network_name or ExampleConfig.get_config_net()
-        local_node = not test_net or test_net in {"local", "ganache"}
-        config_dict = ExampleConfig._get_config(local_node, test_net)
+        config_dict = ExampleConfig._get_config()
         logging.debug(
             f"Configuration loaded for environment `{network_name}`: {config_dict}"
         )
         return config_dict
 
     @staticmethod
-    def get_config(network_name: Optional[str] = None) -> Config:
+    def get_config(network_url: Optional[str] = None) -> Config:
         """
         :return: `Config` instance
         """
-        return Config(options_dict=ExampleConfig.get_config_dict(network_name))
+        return Config(options_dict=ExampleConfig.get_config_dict(network_url))
