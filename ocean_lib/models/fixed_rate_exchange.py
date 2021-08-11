bd3558df27880d82b09ebf264f84174496fae1f7
David Hunt-Mateo <david@oceanprotocol.com>
2021-08-11 11:18:15 -0400

    Update FixedRateExchange docstring to use "wei" terms instead of "base"

--- ocean_lib/models/fixed_rate_exchange.py
+++ ocean_lib/models/fixed_rate_exchange.py
@@ -21,8 +21,8 @@ class FixedRateExchange(ContractBase):
 
     """
 
-    Note: all operations accept and return integer values in base 18 format.
-    Its up to the caller to convert to/from base 18 as necessary.
+    Note: all operations accept and return integer values, denoted in wei
+    Its up to the caller to convert to/from wei as necessary.
 
     """
 
