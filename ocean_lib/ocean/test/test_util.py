43ce8cf79ccff6d9c596988d14b38809f11bc657
mariacarmina <50501033+mariacarmina@users.noreply.github.com>
2021-08-18 12:45:04 +0300

    Update the example_config.py (#435)
    * Updated example config. Added NETWORK_URL env var. Added more fields to the config object. Implemented unit tests for the new config structure.
    * Added changes from (#421).
    * Updated compute-flow README as well. Introduced envvar for the network URL. Added a newline in test_example_config.

--- ocean_lib/ocean/test/test_util.py
+++ ocean_lib/ocean/test/test_util.py
@@ -3,14 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-import os
-
 import pytest
 from ocean_lib.ocean import util
-from ocean_lib.ocean.env_constants import (
-    ENV_INFURA_CONNECTION_TYPE,
-    ENV_INFURA_PROJECT_ID,
-)
 from ocean_lib.ocean.util import (
     from_base,
     from_base_18,
@@ -22,57 +16,7 @@ from ocean_lib.ocean.util import (
 )
 
 
-def test_get_infura_connection_type(monkeypatch):
-    # no envvar
-    if ENV_INFURA_CONNECTION_TYPE in os.environ:
-        monkeypatch.delenv(ENV_INFURA_CONNECTION_TYPE)
-    assert (
-        util.get_infura_connection_type() == "http"
-    ), "The default connection type for infura is not http."
-
-    # envvar is "http"
-    monkeypatch.setenv(ENV_INFURA_CONNECTION_TYPE, "http")
-    assert util.get_infura_connection_type() == "http"
-
-    # envvar is "websocket"
-    monkeypatch.setenv(ENV_INFURA_CONNECTION_TYPE, "websocket")
-    assert util.get_infura_connection_type() == "websocket"
-
-    # envvar is other val
-    monkeypatch.setenv(ENV_INFURA_CONNECTION_TYPE, "foo_type")
-    assert util.get_infura_connection_type() == "http"
-
-
-def test_get_infura_id(monkeypatch):
-    # no envvar
-    if ENV_INFURA_PROJECT_ID in os.environ:
-        monkeypatch.delenv(ENV_INFURA_PROJECT_ID)
-    assert util.get_infura_id() == util.WEB3_INFURA_PROJECT_ID
-
-    # envvar is other val
-    monkeypatch.setenv(ENV_INFURA_PROJECT_ID, "foo_id")
-    assert util.get_infura_id() == "foo_id"
-
-
-def test_get_infura_url(monkeypatch):
-    # envvar is "http"
-    monkeypatch.setenv(ENV_INFURA_CONNECTION_TYPE, "http")
-    assert util.get_infura_url("id1", "net1") == "https://net1.infura.io/v3/id1"
-
-    # envvar is "websocket"
-    monkeypatch.setenv(ENV_INFURA_CONNECTION_TYPE, "websocket")
-    assert util.get_infura_url("id2", "net2") == "wss://net2.infura.io/ws/v3/id2"
-
-    # envvar is other val - it will resort to "http"
-    monkeypatch.setenv(ENV_INFURA_CONNECTION_TYPE, "foo_type")
-    assert util.get_infura_url("id3", "net3") == "https://net3.infura.io/v3/id3"
-
-
 def test_get_web3_connection_provider(monkeypatch):
-    # "ganache"
-    provider = util.get_web3_connection_provider("ganache")
-    assert provider.endpoint_uri == util.GANACHE_URL  # e.g. http://127.0.0.1:8545
-
     # GANACHE_URL
     provider = util.get_web3_connection_provider(util.GANACHE_URL)
     assert provider.endpoint_uri == util.GANACHE_URL
@@ -85,39 +29,8 @@ def test_get_web3_connection_provider(monkeypatch):
     provider = util.get_web3_connection_provider("https://bar.com")
     assert provider.endpoint_uri == "https://bar.com"
 
-    # "rinkeby"
-    assert "rinkeby" in util.SUPPORTED_NETWORK_NAMES
-    monkeypatch.setenv(ENV_INFURA_PROJECT_ID, "id1")
-    provider = util.get_web3_connection_provider("rinkeby")
-    assert provider.endpoint_uri == "https://rinkeby.infura.io/v3/id1"
-
-    # polygon network name
-    assert (
-        "polygon" in util.SUPPORTED_NETWORK_NAMES
-    ), "polygon is missing from SUPPORTED_NETWORK_NAMES"
-    assert util.POLYGON_URL == "https://rpc.polygon.oceanprotocol.com"
-    provider = util.get_web3_connection_provider("polygon")
-    assert provider.endpoint_uri == "https://rpc.polygon.oceanprotocol.com"
-
-    # bsc network name
-    assert (
-        "bsc" in util.SUPPORTED_NETWORK_NAMES
-    ), "bsc is missing from SUPPORTED_NETWORK_NAMES"
-    assert util.BSC_URL == "https://bsc-dataseed.binance.org"
-    provider = util.get_web3_connection_provider("bsc")
-    assert provider.endpoint_uri == "https://bsc-dataseed.binance.org"
-
-    # all infura-supported network names
-    for network in util.SUPPORTED_NETWORK_NAMES:
-        if network == "ganache" or "polygon":
-            continue  # tested above
-        monkeypatch.setenv(ENV_INFURA_PROJECT_ID, f"id_{network}")
-        provider = util.get_web3_connection_provider(network)
-        assert provider.endpoint_uri == f"https://{network}.infura.io/v3/id_{network}"
-
     # non-supported name
-    monkeypatch.setenv(ENV_INFURA_PROJECT_ID, "idx")
-    with pytest.raises(Exception):
+    with pytest.raises(AssertionError):
         util.get_web3_connection_provider("not_network_name")
 
     # typical websockets uri "wss://foo.com"
