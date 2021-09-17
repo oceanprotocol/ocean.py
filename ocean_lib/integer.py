d68a988acd206ddb74bbc2443eda607f8f2e15de
David Hunt-Mateo <david@oceanprotocol.com>
2021-09-17 15:44:33 -0400

    Define explicit __init__ method in Integer

--- ocean_lib/integer.py
+++ ocean_lib/integer.py
@@ -2,12 +2,10 @@
 # Copyright 2021 Ocean Protocol Foundation
 # SPDX-License-Identifier: Apache-2.0
 #
-from dataclasses import dataclass
-
 from enforce_typing.decorator import enforce_types
 
 
-@dataclass
 @enforce_types
 class Integer:
-    value: int
+    def __init__(self, value: int) -> None:
+        self.value = value
