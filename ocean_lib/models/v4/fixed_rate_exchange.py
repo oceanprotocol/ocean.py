58706ff2cd8f91164c22c0c96b8c5200c23e9e6a
mariacarmina <mariacarmina.cretu@gmail.com>
2021-09-29 18:51:22 +0300

    Created a separate package for v4 models. Integrated FRE & factory router interface.

---
+++ ocean_lib/models/v4/fixed_rate_exchange.py
@@ -0,0 +1,67 @@
+#
+# Copyright 2021 Ocean Protocol Foundation
+# SPDX-License-Identifier: Apache-2.0
+#
+from enforce_typing import enforce_types
+
+from ocean_lib.web3_internal.contract_base import ContractBase
+from ocean_lib.web3_internal.wallet import Wallet
+
+
+class FixedRateExchangeV4(ContractBase):
+
+    #########################
+    # Transaction methods
+    @enforce_types
+    def create(
+        self,
+        base_token: str,
+        data_token: str,
+        exchange_rate: int,
+        from_wallet: Wallet,
+        market_fee: int,
+        market_fee_collector: str,
+        opf_fee: int,
+    ) -> str:
+        return self.send_transaction(
+            "create",
+            (
+                base_token,
+                data_token,
+                exchange_rate,
+                from_wallet.address,
+                market_fee,
+                market_fee_collector,
+                opf_fee,
+            ),
+            from_wallet,
+        )
+
+    @enforce_types
+    def createWithDecimals(
+        self,
+        base_token: str,
+        data_token: str,
+        bt_decimals: int,
+        dt_decimals: int,
+        exchange_rate: int,
+        from_wallet: Wallet,
+        market_fee: int,
+        market_fee_collector: str,
+        opf_fee: int,
+    ) -> str:
+        return self.send_transaction(
+            "createWithDecimals",
+            (
+                base_token,
+                data_token,
+                bt_decimals,
+                dt_decimals,
+                exchange_rate,
+                from_wallet.address,
+                market_fee,
+                market_fee_collector,
+                opf_fee,
+            ),
+            from_wallet,
+        )
