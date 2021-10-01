9c82f7c96d3d111d584b1ee0b2fee037afd30012
alexcos20 <alex.coseru@gmail.com>
2021-10-01 03:54:27 -0700

    Bump version: 0.8.2 → 0.8.3

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ setup(
     url="https://github.com/oceanprotocol/ocean.py",
     # fmt: off
     # bumpversion.sh needs single-quotes
-    version='0.8.2',
+    version='0.8.3',
     # fmt: on
     zip_safe=False,
 )
