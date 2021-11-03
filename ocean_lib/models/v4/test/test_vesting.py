66e92664640145c872cc6b01204f1c74e63ffe5b
Berkay Saglam <25263018+trizin@users.noreply.github.com>
2021-11-03 14:21:19 +0300

    Issue542 implement unit tests for vesting v4 (#553)
    * Adds new token and token factories classes.
    
    * Intermediary commit.
    
    * Intermediary commit.
    
    * Intermediary commit.
    
    * Remove support for main barge, make all unit tests pass (except for Aquarius and provider errors).
    
    * Adjust travis to run v4 barge.
    
    * Correct types for token classes.
    
    * Created a separate package for v4 models. Integrated FRE & factory router interface.
    
    * Integrated IFactory, IMetadata, IPool.
    
    * Replace the camelCase with snake_case.
    
    * Integrated all the functions for ERC 721 factory contract. Added a file for named tuples.
    
    * Completed the ERC20 Template contrct by adding the rest of the functions. Fixed some mistakes from ERC721 factory.
    
    * Refactored ERC20 Template and ERC721 Template contracts.
    
    * Fix create ERC20 function. Deleted update_metadata function which doesn't belong to the ERC721 Template contract.
    
    * Created BToken and BFactory contracts interfaces for v4. Added NamedTuple for BPoolData. Small tweaks for ERC721 Factory. Deleted ERC20 Factory file.
    
    * Added BPool and Factory Router contracts. Added touchups to the other contracts.
    
    * Integrated ssFixedRate contract.
    
    * Integrated FixedRateExchange contract.
    
    * Integrated Dispenser contract. Imported typing module for List.
    
    * Added 2 unit tests for ERC721 Factory. Resolved a parameter tweak in deploy ERC721 contract function.
    
    * Added black formatting.
    
    * Added couple unit tests for ERC721 Factory that integrate ERC721 token as well. Added the contracts and other v4 dependencies. Resolved some minor tweaks. Removed fixtures and skip setup_all part for the moment.
    
    * Created v4 testing tutorial. Updated address.json. Updated contract interfaces to respect the newest version of v4 contracts. Refactored CONTRACT_NAME in BTokenBase. Resolved mint_fake_OCEAN issue to accept v4 addresses. Created draft for publishing flow (VERY WIP). Resolved setup_all function from conftest.py.
    
    * Black formatted conftest.py.
    
    * Update artifacts folder with pool contracts. Removed contract addresses' constants. Added helper functions which retrive the addresses from address.json.
    
    * Updated SS contract, Factory Router. Updated README. Created test for test_router.
    
    * Updated address.json. Added test for addOceanToken function.
    
    * Deleted address.json. Updated types for function arguments. Updated pytest.ini and tox.ini.
    
    * wip
    
    * Added the install.sh script back. Updated the ERC721 factory.
    
    * Added utils file for v4 addresses.
    
    * wip
    
    * Try to fix tox.yml for v4 branch from barge.
    
    * merged all tests into one, wip
    
    * Updated artifacts. Implemented ERC721 Factory tests. Added event for Dispenser.
    
    * Added git fetch to tox.yml.
    
    * Refactored a tweak in tox.yml to work with v4 branch from barge.
    
    * Fixed tox.yml to run on v4 barge.
    
    * wip
    
    * wip
    
    * Removed addresses_utils. Updated conftest. Updated helper_functions.
    
    * Revert changes from tox.yml file.
    
    * added helper functions
    
    * code formatting
    
    * using get_address_of_type
    
    * code formatting
    
    * added an assert for the error
    
    * code formatting
    
    * added factory router in conftest file
    
    * code formatting
    
    * tox
    
    * deleted test_router.py
    
    * code formatting + improvements
    
    * code formatting
    
    * replaced user3 with consumer
    replaced erc721_contract with erc721_token
    replaced erc_token with erc20_token
    
    * replaced b_pool with bpool
    
    Co-authored-by: Calina Cenan <calina@cenan.net>
    Co-authored-by: Călina Cenan <calina@kaolin.tech>
    Co-authored-by: mariacarmina <mariacarmina.cretu@gmail.com>

---
+++ ocean_lib/models/v4/test/test_vesting.py
@@ -0,0 +1,183 @@
+#
+# Copyright 2021 Ocean Protocol Foundation
+# SPDX-License-Identifier: Apache-2.0
+#
+import pytest
+from web3 import exceptions
+
+from ocean_lib.models.v4.bpool import BPool
+from ocean_lib.models.v4.erc20_token import ERC20Token
+from ocean_lib.models.v4.erc721_factory import ERC721FactoryContract
+from ocean_lib.models.v4.erc721_token import ERC721Token
+from ocean_lib.models.v4.models_structures import ErcCreateData, PoolData
+from ocean_lib.models.v4.side_staking import SideStaking
+from ocean_lib.web3_internal.constants import ZERO_ADDRESS
+from tests.resources.helper_functions import get_address_of_type
+
+
+def test_main(
+    web3,
+    config,
+    consumer_wallet,
+    publisher_wallet,
+    another_consumer_wallet,
+    factory_router,
+):
+    """Tests main test flow."""
+
+    vesting_amount = web3.toWei("0.0018", "ether")
+
+    erc721_factory = ERC721FactoryContract(
+        web3,
+        get_address_of_type(config, "ERC721Factory"),
+    )
+
+    # Tests deploy erc721
+    tx = erc721_factory.deploy_erc721_contract(
+        "NFT",
+        "NFTS",
+        1,
+        ZERO_ADDRESS,
+        "https://oceanprotocol.com/nft/",
+        publisher_wallet,
+    )
+    tx_receipt = web3.eth.wait_for_transaction_receipt(tx)
+    registered_event = erc721_factory.get_event_log(
+        ERC721FactoryContract.EVENT_NFT_CREATED,
+        tx_receipt.blockNumber,
+        web3.eth.block_number,
+        None,
+    )
+    assert registered_event[0].event == "NFTCreated"
+    assert registered_event[0].args.admin == publisher_wallet.address
+    erc721_token = ERC721Token(
+        web3=web3, address=registered_event[0].args.newTokenAddress
+    )
+
+    symbol = erc721_token.symbol()
+    assert symbol == "NFTS"
+
+    owner_balance = erc721_token.contract.caller.balanceOf(publisher_wallet.address)
+    assert owner_balance == 1
+
+    # Tests roles
+    erc721_token.add_manager(another_consumer_wallet.address, publisher_wallet)
+
+    erc721_token.add_to_create_erc20_list(
+        consumer_wallet.address, another_consumer_wallet
+    )
+    erc721_token.add_to_metadata_list(consumer_wallet.address, another_consumer_wallet)
+    erc721_token.add_to_725_store_list(consumer_wallet.address, another_consumer_wallet)
+
+    permissions = erc721_token.get_permissions(consumer_wallet.address)
+
+    assert permissions[1] == True
+    assert permissions[2] == True
+    assert permissions[3] == True
+
+    # Tests consumer deploys an ERC20DT
+    erc_data = ErcCreateData(
+        1,
+        ["ERC20DT1", "ERC20DT1Symbol"],
+        [
+            consumer_wallet.address,
+            another_consumer_wallet.address,
+            publisher_wallet.address,
+            ZERO_ADDRESS,
+        ],
+        [web3.toWei(0.05, "ether"), 0],
+        [b""],
+    )
+
+    trx_erc_20 = erc721_token.create_erc20(erc_data, consumer_wallet)
+
+    tx_receipt = web3.eth.wait_for_transaction_receipt(trx_erc_20)
+    assert tx_receipt.status == 1
+
+    event = erc721_factory.get_event_log(
+        ERC721Token.EVENT_TOKEN_CREATED,
+        tx_receipt.blockNumber,
+        web3.eth.block_number,
+        None,
+    )
+
+    dt_address = event[0].args.newTokenAddress
+    erc20_token = ERC20Token(web3=web3, address=dt_address)
+
+    # Tests permissions
+    perms = erc20_token.permissions(consumer_wallet.address)
+    assert perms[0] == True
+
+    # Tests consumer deploys pool and check market fee
+    initial_ocean_liq = web3.toWei(0.02, "ether")
+    ocean_contract = ERC20Token(web3=web3, address=get_address_of_type(config, "Ocean"))
+    ocean_contract.approve(
+        get_address_of_type(config, "Router"),
+        web3.toWei(0.02, "ether"),
+        consumer_wallet,
+    )
+
+    pool_data = PoolData(
+        [
+            web3.toWei(1, "ether"),
+            ocean_contract.decimals(),
+            initial_ocean_liq // 100 * 9,
+            2500000,
+            initial_ocean_liq,
+        ],
+        [
+            web3.toWei(0.001, "ether"),
+            web3.toWei(0.001, "ether"),
+        ],
+        [
+            get_address_of_type(config, "Staking"),
+            ocean_contract.address,
+            consumer_wallet.address,
+            consumer_wallet.address,
+            get_address_of_type(config, "OPFCommunityFeeCollector"),
+            get_address_of_type(config, "poolTemplate"),
+        ],
+    )
+    tx = erc20_token.deploy_pool(pool_data, consumer_wallet)
+    tx_receipt = web3.eth.wait_for_transaction_receipt(tx)
+    pool_event = factory_router.get_event_log(
+        ERC721FactoryContract.EVENT_NEW_POOL,
+        tx_receipt.blockNumber,
+        web3.eth.block_number,
+        None,
+    )
+
+    assert pool_event[0].event == "NewPool"
+    bpool_address = pool_event[0].args.poolAddress
+    bpool = BPool(web3, bpool_address)
+    assert bpool.is_finalized() == True
+    assert bpool.opf_fee() == 0
+    assert bpool.get_swap_fee() == web3.toWei(0.001, "ether")
+    assert bpool.community_fee(get_address_of_type(config, "Ocean")) == 0
+    assert bpool.community_fee(erc20_token.address) == 0
+    assert bpool.market_fee(get_address_of_type(config, "Ocean")) == 0
+    assert bpool.market_fee(erc20_token.address) == 0
+
+    assert erc20_token.balanceOf(get_address_of_type(config, "Staking")) == web3.toWei(
+        0.03, "ether"
+    )
+
+    # consumer fails to mint new erc20 token even if the minter
+    perms = erc20_token.permissions(consumer_wallet.address)
+    assert perms[0] == True
+
+    with pytest.raises(exceptions.ContractLogicError) as err:
+        erc20_token.mint(consumer_wallet.address, 1, consumer_wallet)
+        assert (
+            err.value.args[0]
+            == "execution reverted: VM Exception while processing transaction: revert DataTokenTemplate: cap exceeded"
+        )
+
+    assert erc20_token.balanceOf(consumer_wallet.address) == 0
+
+    # check if the vesting amount is correct
+
+    side_staking = SideStaking(
+        web3=web3, address=get_address_of_type(config, "Staking")
+    )
+    assert side_staking.get_vesting_amount(erc20_token.address) == vesting_amount
