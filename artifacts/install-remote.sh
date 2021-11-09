d2e192a7fcc8e1cd7e3af82858351d7da671391c
mariacarmina <50501033+mariacarmina@users.noreply.github.com>
2021-11-09 16:23:32 +0200

    v4 support for CI. (#575)

---
+++ artifacts/install-remote.sh
@@ -0,0 +1,9 @@
+##
+## Copyright 2021 Ocean Protocol Foundation
+## SPDX-License-Identifier: Apache-2.0
+##
+
+rm -rf $VIRTUAL_ENV/lib/python3.8/site-packages/artifacts/
+mkdir ${VIRTUAL_ENV}/lib/python3.8/site-packages/artifacts/
+
+find ./artifacts -name '*.json' -exec cp -prv '{}' ${VIRTUAL_ENV}'/lib/python3.8/site-packages/artifacts/' ';'
--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+##
+## Copyright 2021 Ocean Protocol Foundation
+## SPDX-License-Identifier: Apache-2.0
+##
+
 rm -rf $VIRTUAL_ENV/lib/python3.8/site-packages/artifacts/
 mkdir ${VIRTUAL_ENV}/lib/python3.8/site-packages/artifacts/
 cp ${HOME}/.ocean/ocean-contracts/artifacts/address.json $VIRTUAL_ENV/lib/python3.8/site-packages/artifacts/address.json
