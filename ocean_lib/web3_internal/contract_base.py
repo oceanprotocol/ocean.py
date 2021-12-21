e914696b126e2a4304752ca4bd4c9b32380936c2
David Hunt-Mateo <david@oceanprotocol.com>
2021-12-21 13:46:01 -0500

    Upgrade web3.py to 5.25.0 (v3) (#611)

--- ocean_lib/web3_internal/contract_base.py
+++ ocean_lib/web3_internal/contract_base.py
@@ -209,6 +209,11 @@ class ContractBase(object):
             callback, timeout_callback=timeout_callback, timeout=timeout, blocking=wait
         )
 
+    @staticmethod
+    @enforce_types
+    def get_gas_price(web3) -> int:
+        return int(web3.eth.gas_price * 1.1)
+
     @enforce_types
     def send_transaction(
         self,
@@ -231,6 +236,7 @@ class ContractBase(object):
             "from": from_wallet.address,
             "account_key": from_wallet.key,
             "chainId": self.web3.eth.chain_id,
+            "gasPrice": self.get_gas_price(self.web3),
         }
 
         gas_price = os.environ.get(ENV_GAS_PRICE, None)
@@ -273,7 +279,7 @@ class ContractBase(object):
 
         _contract = web3.eth.contract(abi=_json["abi"], bytecode=_json["bytecode"])
         built_tx = _contract.constructor(*args).buildTransaction(
-            {"from": deployer_wallet.address}
+            {"from": deployer_wallet.address, "gasPrice": cls.get_gas_price(web3)}
         )
         if "chainId" not in built_tx:
             built_tx["chainId"] = web3.eth.chain_id
