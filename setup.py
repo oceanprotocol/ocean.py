c2fdaca3e9328c25194331bee825d7f265a0620f
Maria Carmina <mariacarmina.cretu@gmail.com>
2022-01-31 09:15:09 -0800

    Updated black.

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ dev_requirements = [
     "watchdog",
     "flake8",
     "isort",
-    "black",
+    "black==22.1.0",
     "pre-commit",
     # for the following: maybe needed, maybe not
     "pytest",
