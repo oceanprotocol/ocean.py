bc26fc0f92904bd499958edac3155eed387f6d52
Calina Cenan <calina@cenan.net>
2022-02-08 12:53:25 +0000

    Fix conversion of OrderParams.

--- ocean_lib/models/erc20_enterprise.py
+++ ocean_lib/models/erc20_enterprise.py
@@ -8,7 +8,7 @@ from typing import Union
 from enforce_typing import enforce_types
 
 from ocean_lib.models.erc20_token import ERC20Token
-from ocean_lib.models.models_structures import DispenserData, OrderParams, ProviderFees
+from ocean_lib.models.models_structures import DispenserData, OrderParams
 from ocean_lib.web3_internal.wallet import Wallet
 
 
@@ -23,11 +23,8 @@ class ERC20Enterprise(ERC20Token):
         from_wallet: Wallet,
     ) -> str:
         # TODO: this will be handled in web3 py
-        provider_fees = None
-        if isinstance(order_params[2], ProviderFees):
-            provider_fees = tuple(order_params[2])
-
-        order_params = (order_params[0], order_params[1], provider_fees)
+        if isinstance(order_params, OrderParams):
+            order_params = tuple(order_params)
 
         return self.send_transaction(
             "buyFromFreAndOrder", (order_params, fre_params), from_wallet
@@ -40,11 +37,8 @@ class ERC20Enterprise(ERC20Token):
         from_wallet: Wallet,
     ) -> str:
         # TODO: this will be handled in web3 py
-        provider_fees = None
-        if isinstance(order_params[2], ProviderFees):
-            provider_fees = tuple(order_params[2])
-
-        order_params = (order_params[0], order_params[1], provider_fees)
+        if isinstance(order_params, OrderParams):
+            order_params = tuple(order_params)
 
         return self.send_transaction(
             "buyFromDispenserAndOrder", (order_params, dispenser_address), from_wallet
