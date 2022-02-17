4035b120ee892cd1f289af0fa774a0508fb45d65
Calina Cenan <calina@cenan.net>
2022-02-17 12:33:22 +0000

    Adds Misc coverage to ocean, services and utilities.

--- ocean_lib/services/test/test_service.py
+++ ocean_lib/services/test/test_service.py
@@ -2,15 +2,21 @@
 # Copyright 2022 Ocean Protocol Foundation
 # SPDX-License-Identifier: Apache-2.0
 #
-from ocean_lib.agreements.service_types import ServiceTypes
+from unittest.mock import patch
+
+import pytest
+
 from ocean_lib.assets.asset import Asset
+from ocean_lib.services.service import Service
 from tests.resources.ddo_helpers import get_sample_ddo
 
 
 def test_service():
     """Tests that the get_cost function for ServiceAgreement returns the correct value."""
-    ddo = Asset.from_dict(get_sample_ddo())
-    sa = ddo.get_service(ServiceTypes.ASSET_ACCESS)
+    ddo_dict = get_sample_ddo()
+    service_dict = ddo_dict["services"][0]
+    sa = Service.from_dict(service_dict)
+
     assert sa.id == "1"
     assert sa.name == "Download service"
     assert sa.type == "access"
@@ -27,3 +33,25 @@ def test_service():
         "name": "Download service",
         "description": "Download service",
     }
+
+    ddo_dict = get_sample_ddo()
+    service_dict = ddo_dict["services"][0]
+    del service_dict["type"]
+    with pytest.raises(IndexError):
+        Service.from_dict(service_dict)
+
+    ddo_dict = get_sample_ddo()
+    asset = Asset.from_dict(ddo_dict)
+    with patch(
+        "ocean_lib.services.service.DataServiceProvider.check_asset_file_info"
+    ) as mock:
+        mock.return_value = False
+        assert sa.is_consumable(asset, {}, True) == 2
+
+    with patch(
+        "ocean_lib.services.service.DataServiceProvider.check_asset_file_info"
+    ) as mock:
+        mock.return_value = True
+        assert (
+            sa.is_consumable(asset, {"type": "address", "value": "0xdddd"}, True) == 3
+        )
