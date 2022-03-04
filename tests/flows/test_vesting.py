edd1b0d8c585d13d6b7faf0f1b8125c7a4b848b4
trentmc <5305452+trentmc@users.noreply.github.com>
2022-03-04 09:36:48 +0100

    touch file to start PR

--- tests/flows/test_vesting.py
+++ tests/flows/test_vesting.py
@@ -15,7 +15,7 @@ from ocean_lib.web3_internal.constants import ZERO_ADDRESS
 from ocean_lib.web3_internal.currency import to_wei
 from tests.resources.helper_functions import get_address_of_type
 
-
+ 
 @pytest.mark.unit
 def test_main(
     web3,
