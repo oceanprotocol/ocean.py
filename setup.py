f573a9d44ba6f4c786f0a6f91be240600d71c7a5
Alex Coseru <alex.coseru@gmail.com>
2022-03-21 10:55:46 +0200

    bump to contracts alpha 25

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", encoding="utf8") as readme_file:
 # Installed by pip install ocean-lib
 # or pip install -e .
 install_requirements = [
-    "ocean-contracts==1.0.0a24",
+    "ocean-contracts==1.0.0a25",
     "coloredlogs",
     "pyopenssl",
     "PyJWT",  # not jwt
