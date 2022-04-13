6d09c88aa4cdff685e6ba6cb8661aa66bd3050a4
Maria Carmina <mariacarmina.cretu@gmail.com>
2022-04-13 21:20:11 +0300

    Disconnected aquarius.

--- tests/integration/test_disconnecting_components.py
+++ tests/integration/test_disconnecting_components.py
@@ -2,19 +2,24 @@
 # Copyright 2022 Ocean Protocol Foundation
 # SPDX-License-Identifier: Apache-2.0
 #
+import os
 import threading
 import time
 from unittest.mock import patch
 import requests
 
-from ocean_lib.config import DEFAULT_PROVIDER_URL
+from ocean_lib.config import DEFAULT_PROVIDER_URL, Config
 from ocean_lib.data_provider.data_service_provider import DataServiceProvider
+from ocean_lib.ocean.ocean import Ocean
 from ocean_lib.structures.file_objects import FilesTypeFactory
+from ocean_lib.web3_internal.constants import ZERO_ADDRESS
 
 exception_flag = 0
 
 
 def test_with_wrong_provider(config, caplog):
+    """Tests encrypt with a good provider URL and then switch to a bad one."""
+
     with patch("ocean_lib.config.Config.provider_url") as mock:
         mock.return_value = DEFAULT_PROVIDER_URL
         updating_thread = threading.Thread(
@@ -29,6 +34,62 @@ def test_with_wrong_provider(config, caplog):
         assert exception_flag == 1
 
 
+def test_with_wrong_aquarius(publisher_wallet, caplog, monkeypatch):
+    """Tests DDO creation with a good config.ini and then switch to a bad one."""
+
+    updating_thread = threading.Thread(
+        target=_update_config_file,
+        args=(monkeypatch,),
+    )
+    config = Config(os.getenv("OCEAN_CONFIG_FILE"))
+    ocean = Ocean(config, DataServiceProvider)
+    updating_thread.start()
+    _iterative_create_ddo(ocean, publisher_wallet)
+    updating_thread.join()
+    monkeypatch.delenv("OCEAN_CONFIG_FILE")
+
+    assert "Successfully created NFT" in caplog.text
+    assert exception_flag == 2
+
+
+def _create_ddo(ocean, publisher):
+    global exception_flag
+    metadata = {
+        "created": "2020-11-15T12:27:48Z",
+        "updated": "2021-05-17T21:58:02Z",
+        "description": "Sample description",
+        "name": "Sample asset",
+        "type": "dataset",
+        "author": "OPF",
+        "license": "https://market.oceanprotocol.com/terms",
+    }
+    encrypted_files = _encrypt(DEFAULT_PROVIDER_URL)
+    try:
+        ocean.assets.create(
+            metadata,
+            publisher,
+            encrypted_files,
+            erc20_templates=[1],
+            erc20_names=["Datatoken 1"],
+            erc20_symbols=["DT1"],
+            erc20_minters=[publisher.address],
+            erc20_fee_managers=[publisher.address],
+            erc20_publish_market_order_fee_addresses=[ZERO_ADDRESS],
+            erc20_publish_market_order_fee_tokens=ocean.OCEAN_address,
+            erc20_caps=[ocean.to_wei(100)],
+            erc20_publish_market_order_fee_amounts=[0],
+            erc20_bytess=[[b""]],
+        )
+    except requests.exceptions.SSLError:
+        exception_flag = 2
+
+
+def _iterative_create_ddo(ocean, publisher):
+    for _ in range(5):
+        _create_ddo(ocean, publisher)
+        time.sleep(1)
+
+
 def _encrypt(provider_url):
     global exception_flag
     file_url = "https://foo.txt"
@@ -38,7 +99,7 @@ def _encrypt(provider_url):
 
     # Encrypt file objects
     try:
-        DataServiceProvider.encrypt(files, provider_url)
+        return DataServiceProvider.encrypt(files, provider_url)
     except requests.exceptions.InvalidURL as err:
         exception_flag = 1
         assert err.args[0] == "InvalidURL http://foourl.com."
@@ -53,3 +114,8 @@ def _iterative_encrypt(mock):
 def _update_with_wrong_component(mock):
     time.sleep(2)
     mock.return_value = "http://foourl.com"
+
+
+def _update_config_file(monkeypatch):
+    time.sleep(2)
+    monkeypatch.setenv("OCEAN_CONFIG_FILE", "bad-aqua-config.ini")
