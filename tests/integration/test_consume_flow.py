849d7a00bc612ee05168822e94eb7d1bf6c1b017
David Hunt-Mateo <david@oceanprotocol.com>
2022-04-25 18:16:23 -0400

    Update test_consume_flow, also skip it for now.

--- tests/integration/test_consume_flow.py
+++ tests/integration/test_consume_flow.py
@@ -6,22 +6,29 @@ import os
 import shutil
 
 import pytest
+from web3 import Web3
 
 from ocean_lib.agreements.service_types import ServiceTypes
+from ocean_lib.config import Config
 from ocean_lib.data_provider.data_service_provider import DataServiceProvider
 from ocean_lib.models.erc20_token import ERC20Token
-from ocean_lib.models.erc721_factory import ERC721FactoryContract
+from ocean_lib.models.erc721_nft import ERC721NFT
 from ocean_lib.ocean.ocean_assets import OceanAssets
 from ocean_lib.structures.file_objects import UrlFile
 from ocean_lib.web3_internal.constants import ZERO_ADDRESS
-from ocean_lib.web3_internal.currency import to_wei
+from ocean_lib.web3_internal.currency import parse_units, to_wei
+from ocean_lib.web3_internal.wallet import Wallet
 from tests.resources.ddo_helpers import get_first_service_by_type
-from tests.resources.helper_functions import get_address_of_type
+from tests.resources.helper_functions import (
+    get_address_of_type,
+    transfer_base_token_if_balance_lte,
+)
 
 
 @pytest.mark.integration
+@pytest.mark.skip
 @pytest.mark.parametrize(
-    "base_token_name, publish_market_order_fee_amount",
+    "base_token_name, publish_market_order_fee",
     [
         ("Ocean", "10"),
         ("MockDAI", "10"),
@@ -29,43 +36,27 @@ from tests.resources.helper_functions import get_address_of_type
     ],
 )
 def test_consume_flow(
-    web3,
-    config,
-    publisher_wallet,
-    consumer_wallet,
-    base_token_name,
-    publish_market_order_fee_amount,
+    web3: Web3,
+    config: Config,
+    publisher_wallet: Wallet,
+    consumer_wallet: Wallet,
+    provider_wallet: Wallet,
+    factory_deployer_wallet: Wallet,
+    base_token_name: str,
+    publish_market_order_fee: str,
+    erc721_nft: ERC721NFT,
 ):
     bt = ERC20Token(web3, get_address_of_type(config, base_token_name))
 
-    erc721_factory_address = get_address_of_type(
-        config, ERC721FactoryContract.CONTRACT_NAME
+    # Send base tokens to the consumer so they can pay for fees
+    transfer_base_token_if_balance_lte(
+        web3=web3,
+        base_token_address=bt.address,
+        from_wallet=factory_deployer_wallet,
+        recipient=consumer_wallet.address,
+        min_balance=parse_units("1500", bt.decimals()),
+        amount_to_transfer=parse_units("1500", bt.decimals()),
     )
-    erc721_factory = ERC721FactoryContract(web3, erc721_factory_address)
-
-    # Publisher deploys NFT contract
-    tx = erc721_factory.deploy_erc721_contract(
-        name="NFT1",
-        symbol="NFTSYMBOL",
-        template_index=1,
-        additional_metadata_updater=ZERO_ADDRESS,
-        additional_erc20_deployer=ZERO_ADDRESS,
-        token_uri="https://oceanprotocol.com/nft/",
-        transferable=True,
-        owner=publisher_wallet.address,
-        from_wallet=publisher_wallet,
-    )
-    tx_receipt = web3.eth.wait_for_transaction_receipt(tx)
-    registered_event = erc721_factory.get_event_log(
-        ERC721FactoryContract.EVENT_NFT_CREATED,
-        tx_receipt.blockNumber,
-        web3.eth.block_number,
-        None,
-    )
-
-    assert registered_event[0].event == "NFTCreated"
-    assert registered_event[0].args.admin == publisher_wallet.address
-    erc721_address = registered_event[0].args.newTokenAddress
 
     data_provider = DataServiceProvider
     asset = OceanAssets(config, web3, data_provider)
@@ -88,11 +79,14 @@ def test_consume_flow(
     encrypted_files = encrypt_response.content.decode("utf-8")
 
     # Publish a plain asset with one data token on chain
+    publish_market_order_fee_in_wei = parse_units(
+        publish_market_order_fee, bt.decimals()
+    )
     asset = asset.create(
         metadata=metadata,
         publisher_wallet=publisher_wallet,
         encrypted_files=encrypted_files,
-        erc721_address=erc721_address,
+        erc721_address=erc721_nft.address,
         erc20_templates=[1],
         erc20_names=["Datatoken 1"],
         erc20_symbols=["DT1"],
@@ -100,23 +94,21 @@ def test_consume_flow(
         erc20_fee_managers=[publisher_wallet.address],
         erc20_publish_market_order_fee_addresses=[ZERO_ADDRESS],
         erc20_publish_market_order_fee_tokens=[bt.address],
-        erc20_caps=[to_wei(100)],
-        erc20_publish_market_order_fee_amounts=[publish_market_order_fee_amount],
+        erc20_caps=[to_wei(100)],  # Doesn't matter, DT cap is always MAX_WEI
+        erc20_publish_market_order_fee_amounts=[publish_market_order_fee_in_wei],
         erc20_bytess=[[b""]],
     )
 
     assert asset, "The asset is not created."
     assert asset.nft["name"] == "NFT1"
     assert asset.nft["symbol"] == "NFTSYMBOL"
-    assert asset.nft["address"] == erc721_address
+    assert asset.nft["address"] == erc721_nft.address
     assert asset.nft["owner"] == publisher_wallet.address
     assert asset.datatokens[0]["name"] == "Datatoken 1"
     assert asset.datatokens[0]["symbol"] == "DT1"
 
     service = get_first_service_by_type(asset, ServiceTypes.ASSET_ACCESS)
     dt = ERC20Token(web3, asset.datatokens[0]["address"])
-
-    assert dt.get_erc721_address()
 
     # Mint 50 ERC20 tokens in consumer wallet from publisher. Max cap = 100
     dt.mint(
@@ -126,6 +118,12 @@ def test_consume_flow(
     )
 
     # Check balances
+    publisher_bt_balance_before = bt.balanceOf(publisher_wallet.address)
+    publisher_dt_balance_before = dt.balanceOf(publisher_wallet.address)
+    consumer_bt_balance_before = bt.balanceOf(consumer_wallet.address)
+    consumer_dt_balance_before = dt.balanceOf(consumer_wallet.address)
+    provider_bt_balance_before = bt.balanceOf(provider_wallet.address)
+    provider_dt_balance_before = dt.balanceOf(provider_wallet.address)
 
     # Initialize service
     response = data_provider.initialize(
@@ -153,6 +151,31 @@ def test_consume_flow(
         consume_market_order_fee_amount=0,
         from_wallet=consumer_wallet,
     )
+
+    # Get balances
+    publisher_bt_balance_after_order = bt.balanceOf(publisher_wallet.address)
+    publisher_dt_balance_after_order = dt.balanceOf(publisher_wallet.address)
+    consumer_bt_balance_after_order = bt.balanceOf(consumer_wallet.address)
+    consuemr_dt_balance_after_order = dt.balanceOf(consumer_wallet.address)
+    provider_bt_balance_after_order = bt.balanceOf(provider_wallet.address)
+    provider_dt_balance_after_order = dt.balanceOf(provider_wallet.address)
+
+    # Get order fee amount
+    publish_market_order_fee_amount = dt.get_publishing_market_fee()[2]
+
+    # Check balances
+    assert (
+        publisher_bt_balance_after_order + publish_market_order_fee_amount
+        == publisher_bt_balance_after_order
+    )
+    assert publisher_dt_balance_before + to_wei(1) == publisher_dt_balance_after_order
+    assert (
+        consumer_bt_balance_before - publish_market_order_fee_amount
+        == consumer_bt_balance_after_order
+    )
+    assert consumer_dt_balance_before - to_wei(1) == consuemr_dt_balance_after_order
+    assert provider_bt_balance_before == provider_bt_balance_after_order
+    assert provider_dt_balance_before == provider_dt_balance_after_order
 
     # Download file
     destination = config.downloads_path
@@ -183,3 +206,13 @@ def test_consume_flow(
     assert len(
         os.listdir(os.path.join(destination, os.listdir(destination)[0]))
     ) == len(files), "The asset folder is empty."
+
+    # Get balances
+    publisher_bt_balance_after_download = bt.balanceOf(publisher_wallet.address)
+    publisher_dt_balance_after_download = dt.balanceOf(publisher_wallet.address)
+    consumer_bt_balance_after_download = bt.balanceOf(consumer_wallet.address)
+    consuemr_dt_balance_after_download = dt.balanceOf(consumer_wallet.address)
+    provider_bt_balance_after_download = bt.balanceOf(provider_wallet.address)
+    provider_dt_balance_after_download = dt.balanceOf(provider_wallet.address)
+
+    # Check balances and provider fees
