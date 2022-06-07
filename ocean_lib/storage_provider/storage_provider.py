0de44d7ffb3f0dfa5068164036db580b2b788117
Richard Blythman <richardblythman@gmail.com>
2022-06-07 16:36:48 +0100

    Remove HuggingFace functionality from storage provider

--- ocean_lib/storage_provider/storage_provider.py
+++ ocean_lib/storage_provider/storage_provider.py
@@ -17,8 +17,6 @@ from ocean_lib.config import Config
 from ocean_lib.exceptions import StorageProviderException
 from ocean_lib.http_requests.requests_session import get_requests_session
 
-from ocean_lib.storage_provider.huggingface_hub import upload_to_hf_hub
-
 logger = logging.getLogger(__name__)
 
 class StorageProvider:
@@ -29,51 +27,40 @@ class StorageProvider:
         self.storage_url = self.config.storage_url
         self.storage_type = get_storage_type(self.storage_url)
         self.requests_session = get_requests_session()
-        self.payload_key = {"estuary" : "data", "web3_storage" : "file"}
+        self.payload_key = {"estuary" : "data", "web3.storage" : "file"}
 
     @enforce_types
-    def upload(
-        self, 
-        object_path: str, 
-        object_name: str, 
-        object_type: str = "model",
-        exist_ok = True
-        ) -> Response:
+    def upload(self, object_path: str) -> Response:
         
         path = Path(object_path)
         with open(path, "rb") as f:
             object_to_upload = f.read()
 
-        if self.storage_type == "web3.storage" or self.storage_type == "estuary":
-            response = self.requests_session.post(
-                self.storage_url,
-                files={self.payload_key[self.storage_type]: object_to_upload},
-                headers={"Authorization": "Bearer " + os.environ["STORAGE_TOKEN"]},
+        response = self.requests_session.post(
+            self.storage_url,
+            files={self.payload_key[self.storage_type]: object_to_upload},
+            headers={"Authorization": "Bearer " + os.environ["STORAGE_TOKEN"]},
+        )
+
+        if not hasattr(response, "status_code"):
+            raise StorageProviderException(
+                f"Failed to get a response for request: storageEndpoint={self.storage_url}, response is {response}"
             )
 
-            if not response or not hasattr(response, "status_code"):
-                raise StorageProviderException(
-                    f"Failed to get a response for request: storageEndpoint={self.storage_url}, response is {response}"
-                )
-
-            if response.status_code != 200:
-                msg = (
-                    f"Upload file failed at the storageEndpoint "
-                    f"{self.storage_url}, reason {response.text}, status {response.status_code}"
-                )
-                logger.error(msg)
-                raise StorageProviderException
-
-            logger.info(
-                f"Asset urls encrypted successfully, encrypted urls str: {response.text},"
-                f" encryptedEndpoint {self.storage_url}"
+        if response.status_code != 200:
+            msg = (
+                f"Upload file failed at the storageEndpoint "
+                f"{self.storage_url}, reason {response.text}, status {response.status_code}"
             )
+            logger.error(msg)
+            raise StorageProviderException
 
-            return response
+        logger.info(
+            f"Asset urls encrypted successfully, encrypted urls str: {response.text},"
+            f" encryptedEndpoint {self.storage_url}"
+        )
 
-        elif self.storage_type == "huggingface":
-            repo_url = upload_to_hf_hub(object_path, object_name, object_type, exist_ok=exist_ok)
-            return repo_url
+        return response
 
     @enforce_types
     def download(cid: str) -> Response:
@@ -92,6 +79,5 @@ class StorageProvider:
 def get_storage_type(storage_url):
     domain = storage_url.split('/')[2]
     domain_to_type_dict = {'api.web3.storage':'web3.storage', 
-                            'api.estuary.tech':'estuary', 
-                            'huggingface.co':'huggingface'}
+                            'api.estuary.tech':'estuary'}
     return domain_to_type_dict[domain]
