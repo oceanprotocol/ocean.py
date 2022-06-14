cd913856b00fd1b691f9afbe0d59373238ccd751
Alex Coseru <alex.coseru@gmail.com>
2022-06-14 20:54:14 +0300

    bump to contracts 1.0.0 (#851)
    * bump to contracts 1.0.0
    * Use free c2d environment.
    * Fix DSP test.

--- tests/stress/compute_flow_with_threads.py
+++ tests/stress/compute_flow_with_threads.py
@@ -165,7 +165,9 @@ def c2d_flow_readme(
     compute_service = get_first_service_by_type(DATA_asset, "compute")
     algo_service = get_first_service_by_type(ALGO_asset, "access")
 
-    environments = ocean.compute.get_c2d_environments(compute_service.service_endpoint)
+    free_c2d_env = ocean.compute.get_free_c2d_environment(
+        compute_service.service_endpoint
+    )
 
     DATA_compute_input = ComputeInput(DATA_did, compute_service.id)
     ALGO_compute_input = ComputeInput(ALGO_did, algo_service.id)
@@ -176,9 +178,9 @@ def c2d_flow_readme(
         algorithm_data=ALGO_compute_input,
         consume_market_order_fee_address=consumer_wallet.address,
         wallet=consumer_wallet,
-        compute_environment=environments[0]["id"],
+        compute_environment=free_c2d_env["id"],
         valid_until=int((datetime.utcnow() + timedelta(days=1)).timestamp()),
-        consumer_address=environments[0]["consumerAddress"],
+        consumer_address=free_c2d_env["consumerAddress"],
     )
     assert datasets, "pay for dataset unsuccessful"
     assert algorithm, "pay for algorithm unsuccessful"
@@ -187,7 +189,7 @@ def c2d_flow_readme(
     job_id = ocean.compute.start(
         consumer_wallet=consumer_wallet,
         dataset=datasets[0],
-        compute_environment=environments[0]["id"],
+        compute_environment=free_c2d_env["id"],
         algorithm=algorithm,
     )
     assert job_id, "start compute unsuccessful"
