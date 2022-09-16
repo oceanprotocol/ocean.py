2ffde86c5f965a8555c23467f1714dc5be68931e
trentmc <gtrent@gmail.com>
2022-09-16 13:10:26 -0600

    split test into two: one for non-ocean (send eth), and one for ocean (create dat nft). The non-ocean test passes. WIP on ocean test

--- tests/integration/test_mumbai.py
+++ tests/integration/test_mumbai.py
@@ -7,10 +7,69 @@ from ocean_lib.ocean.ocean import Ocean
 from ocean_lib.ocean.util import networkToChainId
 from ocean_lib.web3_internal.wallet import Wallet
 
+def test_nonocean_tx(tmp_path):
+    """Do a simple non-Ocean tx on Mumbai. Only use Ocean config"""
     
-#this decorator skips ../../conftest.py. Why: we don't want it to turn on on ganache
-@pytest.mark.nosetup_all
-def test1(tmp_path):
+    # setup
+    config = _remote_config(tmp_path)
+    ocean = Ocean(config)
+    (alice_wallet, bob_wallet) = _get_wallets(ocean)
+
+    # Get gas price (in Gwei) from Polygon gas station
+    gas_price = requests.get('https://gasstation-mumbai.matic.today/v2').json()['fast']['maxFee']
+
+    # Simplest possible tx: Alice send Bob some fake MATIC
+    web3 = ocean.web3
+    bob_eth_before = web3.eth.get_balance(bob_wallet.address)
+    nonce = web3.eth.getTransactionCount(alice_wallet.address)
+    tx = {'nonce': nonce,
+          'gasPrice': web3.toWei(gas_price, 'gwei'),
+          'gas': 21000, #a standard ETH transfer needs 21K gas
+          'chainId': networkToChainId("mumbai"),
+          'to': bob_wallet.address,
+          'from' : alice_wallet.address,
+          'value': web3.toWei(0.001, 'ether'),
+          }
+    signed_tx = web3.eth.account.sign_transaction(tx, alice_wallet.private_key)
+
+    print("Do a send-Ether tx...")
+    tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    
+    print("Wait for send-Ether tx to complete...")
+    tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
+    
+    bob_eth_after = web3.eth.get_balance(bob_wallet.address)
+    assert bob_eth_after > bob_eth_before
+    
+def test_ocean_tx(tmp_path):
+    """Do a (simple) Ocean tx on Mumbai"""
+    
+    # setup
+    config = _remote_config(tmp_path)
+    ocean = Ocean(config)
+    (alice_wallet, _) = _get_wallets(ocean)
+    
+    # Alice publish data NFT
+    print("Do an Ocean tx, and wait for it to complete...")
+    data_nft = ocean.create_data_nft('My NFT1', 'NFT1', alice_wallet)
+    assert data_nft.symbol() == 'NFT1'
+
+
+def _get_wallets(ocean):
+    config, web3 = ocean.config, ocean.web3
+    
+    alice_private_key = os.getenv('REMOTE_TEST_PRIVATE_KEY1')
+    bob_private_key = os.getenv('REMOTE_TEST_PRIVATE_KEY2')
+
+    # wallets
+    alice_wallet = Wallet(web3, alice_private_key, config.block_confirmations, config.transaction_timeout)
+    bob_wallet = Wallet(web3, bob_private_key, config.block_confirmations, config.transaction_timeout)
+    print(f"alice_wallet.address = '{alice_wallet.address}'")
+    print(f"bob_wallet.address = '{bob_wallet.address}'")
+
+    return (alice_wallet, bob_wallet)
+
+def _remote_config(tmp_path):
     # Ensure no unwanted envvars
     os.environ.pop("OCEAN_NETWORK_URL", None)
     os.environ.pop("METADATA_CACHE_URI", None)
@@ -47,45 +106,4 @@ provider.url = https://v4.provider.mumbai.oceanprotocol.com
     print(f"config.metadata_cache_uri = '{config.metadata_cache_uri}'")
     print(f"config.provider_url = '{config.provider_url}'")
 
-    # Create Ocean instance
-    ocean = Ocean(config)
-    web3 = ocean.web3
-
-    # Create Alice's wallet
-    alice_private_key = os.getenv('REMOTE_TEST_PRIVATE_KEY1')
-    alice_wallet = Wallet(web3, alice_private_key, config.block_confirmations, config.transaction_timeout)
-    print(f"alice_wallet.address = '{alice_wallet.address}'")
-
-    # Create Bob's wallet
-    bob_private_key = os.getenv('REMOTE_TEST_PRIVATE_KEY2')
-    bob_wallet = Wallet(web3, bob_private_key, config.block_confirmations, config.transaction_timeout)
-    print(f"bob_wallet.address = '{bob_wallet.address}'")
-
-    # Get gas price (in Gwei) from Polygon gas station
-    gas_price = requests.get('https://gasstation-mumbai.matic.today/v2').json()['fast']['maxFee']
-
-    # Simplest possible tx: Alice send Bob some fake MATIC
-    bob_eth_before = web3.eth.get_balance(bob_wallet.address)
-    nonce = web3.eth.getTransactionCount(alice_wallet.address)
-    tx = {'nonce': nonce,
-          'gasPrice': web3.toWei(gas_price, 'gwei'),
-          'gas': 21000, #a standard ETH transfer needs 21K gas
-          'chainId': networkToChainId("mumbai"),
-          'to': bob_wallet.address,
-          'from' : alice_wallet.address,
-          'value': web3.toWei(0.001, 'ether'),
-          }
-    signed_tx = web3.eth.account.sign_transaction(tx, alice_wallet.private_key)
-    print("Do a send-Ether tx...")
-    tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
-    print("Wait for send-Ether tx to complete...")
-    tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
-    bob_eth_after = web3.eth.get_balance(bob_wallet.address)
-    assert bob_eth_after > bob_eth_before
-    
-    # Super-simple Ocean tx: Alice publish data NFT
-    import pdb; pdb.set_trace()
-    print("Do an Ocean tx, and wait for it to complete...")
-    data_nft = ocean.create_data_nft('My NFT1', 'NFT1', alice_wallet)
-    assert data_nft.symbol() == 'NFT1'
-
+    return config
