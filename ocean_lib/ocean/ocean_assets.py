5573a1f703b6b907d3d8ef52003373edb0bd9130
trentmc <gtrent@gmail.com>
2022-10-14 12:21:35 -0500

    tweak

--- ocean_lib/ocean/ocean_assets.py
+++ ocean_lib/ocean/ocean_assets.py
@@ -756,15 +756,15 @@ class OceanAssets:
             dispenser_addr = get_address_of_type(self._config_dict, "Dispenser")
             dispenser = Dispenser(self._web3, dispenser_addr)
 
-            #catch key failure modes
+            # catch key failure modes
             st = dispenser.status(datatoken.address)
             active, disp_bal, allowedSwapper = st[0], from_wei(st[5]), st[6]
             if not active:
                 raise ValueError("No active dispenser for datatoken")
-            if disp_bal < 1.0: 
+            if disp_bal < 1.0:
                 raise ValueError("Dispenser has <1 tokens left. ({disp_bal})")
             if allowedSwapper not in [ZERO_ADDRESS, wallet.address]:
-                raise ValueError("Not allowed. allowedSwapper={allowedSwapper}") 
+                raise ValueError("Not allowed. allowedSwapper={allowedSwapper}")
             # Try to dispense. If other issues, they'll pop out
             dispenser.dispense_tokens(datatoken, amt_dispense_wei, wallet)
 
