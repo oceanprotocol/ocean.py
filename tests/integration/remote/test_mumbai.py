cacda5f4542c60be086a814730de4255dc280488
Calina Cenan <calina@cenan.net>
2022-10-20 09:23:08 +0000

    Use simple send ether in mumbai test.

--- tests/integration/remote/test_mumbai.py
+++ tests/integration/remote/test_mumbai.py
@@ -2,13 +2,10 @@
 # Copyright 2022 Ocean Protocol Foundation
 # SPDX-License-Identifier: Apache-2.0
 #
-import random
-import time
 import warnings
 
-import requests
-
 from ocean_lib.ocean.ocean import Ocean
+from ocean_lib.web3_internal.transactions import send_ether
 
 from .util import get_wallets, random_chars
 
@@ -21,41 +18,19 @@ def test_nonocean_tx(tmp_path):
     ocean = Ocean(config)
     (alice_wallet, bob_wallet) = get_wallets(ocean)
 
-    # Get gas price (in Gwei) from Polygon gas station
-    gas_station_url = "https://gasstation-mumbai.matic.today/v2"
-    gas_price = requests.get(gas_station_url).json()["fast"]["maxFee"]
-
     # Simplest possible tx: Alice send Bob some fake MATIC
     # TODO: replace with send_ether?
     web3 = ocean.web3
     bob_eth_before = web3.eth.get_balance(bob_wallet.address)
-    nonce = web3.eth.getTransactionCount(alice_wallet.address)
-
-    # avoid "replacement transaction underpriced" error: make each tx diff't
-    normalized_unixtime = time.time() / 1e9
-    amt_send = 1e-8 * (random.random() + normalized_unixtime)
-    tx = {
-        "nonce": nonce,
-        "gasPrice": web3.toWei(gas_price, "gwei"),
-        "gas": 21000,  # a standard ETH transfer needs 21K gas
-        "chainId": web3.eth.chain_id,
-        "to": bob_wallet.address,
-        "from": alice_wallet.address,
-        "value": web3.toWei(amt_send, "ether"),
-    }
-    signed_tx = web3.eth.account.sign_transaction(tx, alice_wallet.private_key)
 
     print("Do a send-Ether tx...")
     try:  # it can get away with "insufficient funds" errors, but not others
-        tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+        send_ether(config, alice_wallet, bob_wallet.address, "0.0001 ether")
     except ValueError as error:
         if "insufficient funds" in str(error):
             warnings.warn(UserWarning("Warning: Insufficient test MATIC"))
             return
         raise (error)
-
-    print("Wait for send-Ether tx to complete...")
-    _ = web3.eth.wait_for_transaction_receipt(tx_hash)
 
     bob_eth_after = web3.eth.get_balance(bob_wallet.address)
     assert bob_eth_after > bob_eth_before
