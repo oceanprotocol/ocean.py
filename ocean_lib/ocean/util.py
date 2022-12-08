07c42113069a2e99b52a798cc09d1e8fb977775c
trentmc <gtrent@gmail.com>
2022-12-08 05:44:36 -0300

    Fix #1149 - Streamline fixed-rate-exchange & marketplace README (branch 2)

--- ocean_lib/ocean/util.py
+++ ocean_lib/ocean/util.py
@@ -44,3 +44,14 @@ def create_checksum(text: str) -> str:
     :return: str
     """
     return hashlib.sha256(text.encode("utf-8")).hexdigest()
+
+
+
+@enforce_types
+def from_wei(amt_wei: int):
+    return float(amt_wei / 1e18)
+
+
+@enforce_types
+def to_wei(amt_eth) -> int:
+    return int(amt_eth * 1e18)
