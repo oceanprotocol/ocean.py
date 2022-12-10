5ddb8473437b800f5cf7d831b184ee2e738d8352
Trent McConaghy <gtrent@gmail.com>
2022-12-10 11:16:44 -0300

    Fix #1174 - Exception: Address not found for development. Please check your address file (#1175)
    - Fixes #1174
    - Renames `get_addresses_with_fallback` -> `get_contracts_addresses_all_networks`, to show contrast to `get_contracts_addresses`

--- conftest_ganache.py
+++ conftest_ganache.py
@@ -11,7 +11,7 @@ from ocean_lib.models.data_nft_factory import DataNFTFactoryContract
 from ocean_lib.models.datatoken import Datatoken
 from ocean_lib.models.factory_router import FactoryRouter
 from ocean_lib.ocean.util import get_address_of_type
-from ocean_lib.web3_internal.contract_utils import get_addresses_with_fallback
+from ocean_lib.web3_internal.contract_utils import get_contracts_addresses_all_networks
 from ocean_lib.web3_internal.utils import connect_to_network
 from tests.resources.helper_functions import (
     get_another_consumer_wallet,
@@ -47,7 +47,7 @@ def setup_all(request, config, ocean_token):
     if not wallet:
         return
 
-    if not get_addresses_with_fallback(config):
+    if not get_contracts_addresses_all_networks(config):
         print("Can not find adddresses.")
         return
 
