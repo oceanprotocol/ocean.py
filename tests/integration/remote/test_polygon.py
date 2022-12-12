198643e251ac73a8d7fd68318f2f186876228a36
Maria Carmina <mariacarmina.cretu@gmail.com>
2022-12-12 17:10:05 +0200

    removed POA middleware.

--- tests/integration/remote/test_polygon.py
+++ tests/integration/remote/test_polygon.py
@@ -2,10 +2,8 @@
 # Copyright 2022 Ocean Protocol Foundation
 # SPDX-License-Identifier: Apache-2.0
 #
-import brownie
 import pytest
 from brownie.network import accounts
-from web3.middleware import geth_poa_middleware
 
 from ocean_lib.example_config import get_config_dict
 from ocean_lib.ocean.ocean import Ocean
@@ -20,7 +18,6 @@ def test_ocean_tx__create_data_nft(tmp_path, monkeypatch):
     monkeypatch.delenv("ADDRESS_FILE")
     # setup
     connect_to_network("polygon")
-    brownie.web3.middleware_stack.inject(geth_poa_middleware, layer=0)
     util.set_aggressive_gas_fees()
 
     config = get_config_dict("polygon")
