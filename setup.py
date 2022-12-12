8ed7e1a14809598e38df4188b68967dc76bca6e8
Maria Carmina <mariacarmina.cretu@gmail.com>
2022-12-12 13:38:24 +0200

    Updated black version.

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ dev_requirements = [
     "watchdog",
     "flake8==5.0.4",
     "isort==5.10.1",
-    "black==22.6.0",  # need to keep this up to date to brownie
+    "black==22.10.0",  # need to keep this up to date to brownie
     "pre-commit",
     # for the following: maybe needed, maybe not
     "pytest",
