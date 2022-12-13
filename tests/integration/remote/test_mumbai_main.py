82b0fddb5a5e342fe8ad7d3e8ae4e1841ace7e65
mariacarmina <50501033+mariacarmina@users.noreply.github.com>
2022-12-13 03:21:56 -0800

    Adapt to brownie default config file (#1167)
    * Document brownie network namings in ocean.py. Changed polygon into polygon-main & mumbai into polygon-test. Updated tests. Merged main branch. Updated contract-utils function.
    
    * Updated CI workflow.
    
    * Updated example config and simple remote README.
    
    * Updated polygon RPCs.
    
    * tweak.
    
    * Find .brownie path.
    
    * Find file from brownie.
    
    * Added home path for CI.
    
    * Find file path.
    
    * Find file path v2.
    
    * Print python path.
    
    * Added copy for default brownie config.
    
    * Removed comment from yml file.
    
    * Changed network names from tests and print config file.
    
    * Added in place option for saving the file after sed.
    
    * Replaced mumbai with polygon-test.
    
    * Removed cat command from worflow.
    
    * Added tweak.
    
    * Resolved doc suggestions. Deleted copy file.
    
    * Updated tweaks.

--- tests/integration/remote/test_mumbai_main.py
+++ tests/integration/remote/test_mumbai_main.py
@@ -15,10 +15,10 @@ def test_nonocean_tx(tmp_path, monkeypatch):
     """Do a simple non-Ocean tx on Mumbai. Only use Ocean config"""
     monkeypatch.delenv("ADDRESS_FILE")
     # setup
-    connect_to_network("mumbai")
+    connect_to_network("polygon-test")
     util.set_aggressive_gas_fees()
 
-    config = get_config_dict("mumbai")
+    config = get_config_dict("polygon-test")
     ocean = Ocean(config)
     accounts.clear()
     (alice_wallet, bob_wallet) = util.get_wallets()
@@ -31,10 +31,10 @@ def test_ocean_tx__create_data_nft(tmp_path, monkeypatch):
     """On Mumbai, do a simple Ocean tx: create_data_nft"""
     monkeypatch.delenv("ADDRESS_FILE")
     # setup
-    connect_to_network("mumbai")
+    connect_to_network("polygon-test")
     util.set_aggressive_gas_fees()
 
-    config = get_config_dict("mumbai")
+    config = get_config_dict("polygon-test")
     ocean = Ocean(config)
 
     accounts.clear()
