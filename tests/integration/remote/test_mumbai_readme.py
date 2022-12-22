eaf44ac8b4bde30260c4dbebad512e8bd571f243
Calina Cenan <calina@cenan.net>
2022-12-22 18:45:02 +0000

    Revert "Remove mumbai readme test."
    This reverts commit 724aadbbfd566bd76ed20207c219f44872612349.

---
+++ tests/integration/remote/test_mumbai_readme.py
@@ -0,0 +1,42 @@
+#
+# Copyright 2022 Ocean Protocol Foundation
+# SPDX-License-Identifier: Apache-2.0
+#
+import pathlib
+import runpy
+
+from brownie.network import accounts
+
+from . import util
+
+
+def test_simple_remote_readme(monkeypatch):
+    monkeypatch.delenv("ADDRESS_FILE")
+    accounts.clear()
+    (ref_alice_wallet, _) = util.get_wallets()
+
+    # README generation command:
+    # mkcodes --github --output tests/generated-readmes/test_{name}.{ext} READMEs
+    script = pathlib.Path(
+        __file__, "..", "..", "..", "generated-readmes", "test_setup-remote.py"
+    )
+
+    try:
+        result = runpy.run_path(str(script), run_name="__main__")
+    except AssertionError as e:  # skip if zero funds in account
+        if "Alice needs MATIC" in str(e) or "Bob needs MATIC" in str(e):
+            return
+        raise (e)
+    ocean = result["ocean"]
+    alice = result["alice"]
+
+    # at this point, this script should have set up ocean and the wallets
+
+    # make sure that the script used REMOTE_TEST_PRIVATE_KEY1 wallet, like reference wallet
+    assert alice.address == ref_alice_wallet.address
+
+    # ensure we pay enough
+    util.set_aggressive_gas_fees()
+
+    # besides what the readme script does, is it actually able to do more?
+    util.do_ocean_tx_and_handle_gotchas(ocean, alice)
