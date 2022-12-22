553d8a8e32dd95be627be033f6611568f4e57571
Calina Cenan <calina@cenan.net>
2022-12-22 10:56:53 +0000

    Readme fixes.

--- tests/readmes/test_readmes.py
+++ tests/readmes/test_readmes.py
@@ -22,78 +22,48 @@ def test_script_execution(script, monkeypatch):
     # local setup + main-flow, remote setup + remote main-flow, c2d flow
     # maybe with setup: publish-flow-graphql, same for onchain and REST API?
 
-    if (
-        "developers" in script.name
-        or "df" in script.name
-        or "publish-flow" in script.name
-        or "data-nfts-and-datatokens-flow" in script.name
-        or "c2d-flow-more-examples" in script.name
-        or "parameters" in script.name
-    ):
-        # developers.md skipped because it does not have end-to-end Python snippets, just console
-        # df.md -- ditto
-        # data-nfts-and-datatokens-flow.md and publish-flow skipped because it they run as prerequisites for the others, so they are tested implicitly
-        # c2d-flow-more-examples skipped because it can not be parsed separately from c2d-flow
+    skippable = [
+        "c2d-flow-more-examples",
+        "developers",
+        "df",
+        "install",
+        "parameters",
+        "predict-eth",
+        "services",
+        "setup-local",
+        "setup-remote",
+    ]
+
+    if script.name.replace("test_", "").replace(".py", "") in skippable:
         return
 
-    if (
-        "predict-eth" in script.name
-        or "simple-remote" in script.name
-        or "c2d-flow" in script.name
-    ):
-        # TODO: implement remote flows readme tests
-        # C2D FLOW IS NOW REMOTE. Can we have a local one?
-        return
-
-    runs_with_prerequisites = [
-        "c2d-flow",
-        "dispenser-flow",
-        "marketplace-flow",
-        "key-value-flow",
+    runs_with_local_setup = [
         "profile-nfts-flow",
-        "consume-flow",
+        "key-value-flow",
         "search-and-filter-assets",
+        "main-flow",
+        "c2d-flow",
+        "publish-flow-graphql",
+        "publish-flow-onchain",
+        "publish-flow-restapi",
     ]
 
     globs = {}
-    for item in runs_with_prerequisites:
+    for item in runs_with_local_setup:
         if item in script.name:
             prerequisite = pathlib.Path(
                 __file__,
                 "..",
                 "..",
-                "generated-readmes/test_data-nfts-and-datatokens-flow.py",
+                "generated-readmes/test_setup-local.py",
             )
             result = runpy.run_path(str(prerequisite), run_name="__main__")
             for key in [
                 "os",
                 "config",
                 "ocean",
-                "alice_wallet",
-                "bob_wallet",
-                "data_nft",
-                "datatoken",
-            ]:
-                globs[key] = result[key]
-
-    runs_with_publish = [
-        "consume-flow",
-    ]
-    for item in runs_with_publish:
-        if item in script.name:
-            prerequisite = pathlib.Path(
-                __file__,
-                "..",
-                "..",
-                "generated-readmes/test_publish-flow.py",
-            )
-            result = runpy.run_path(
-                str(prerequisite), run_name="__main__", init_globals=globs
-            )
-            for key in [
-                "ddo",
-                "metadata",
-                "url_file",
+                "alice",
+                "bob",
             ]:
                 globs[key] = result[key]
 
