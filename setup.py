1cbb692268272c05b8e53efa6819ce38aa73bd5c
Trent McConaghy <gtrent@gmail.com>
2022-12-26 16:47:04 -0300

    Bump version 2.0.0-alpha.7 -> 2.0.0-alpha.8 (#1244)

--- setup.py
+++ setup.py
@@ -104,7 +104,7 @@ setup(
     url="https://github.com/oceanprotocol/ocean.py",
     # fmt: off
     # bumpversion.sh needs single-quotes
-    version='2.0.0-alpha.7',
+    version='2.0.0-alpha.8',
     # fmt: on
     zip_safe=False,
 )
