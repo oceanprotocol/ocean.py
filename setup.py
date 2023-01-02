220e85d8b38c7d31b15a73829e207518a3493b6c
Calina Cenan <calina@cenan.net>
2023-01-02 08:36:41 +0000

    Try not enforcing yarl version and relying on brownie dependency.

--- setup.py
+++ setup.py
@@ -33,8 +33,7 @@ install_requirements = [
     "enforce-typing==1.0.0.post1",
     "json-sempai==0.4.0",
     "eciespy",
-    "eth-brownie",
-    "yarl==1.8.2",
+    "eth-brownie==1.19.2",
     "bitarray>=2.6.0,<3",
     # web3 requires eth-abi, requests, and more,
     # so those will be installed too.
