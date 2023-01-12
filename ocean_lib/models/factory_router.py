2c73ccfc5e91f5fb525d91860616089af0be8f03
Calina Cenan <calina@cenan.net>
2023-01-12 15:48:59 +0000

    Remove Factory router inventory of functions.

--- ocean_lib/models/factory_router.py
+++ ocean_lib/models/factory_router.py
@@ -4,30 +4,5 @@
 #
 from ocean_lib.web3_internal.contract_base import ContractBase
 
-"""
-approvedTokens
-balance
-buyDTBatch
-consumeFee
-getApprovedTokens
-getDispensersContracts
-getFixedRatesContracts
-getMinVestingPeriod
-getOPCCollector
-getOPCConsumeFee
-getOPCFee
-getOPCFees
-getOPCProviderFee
-isApprovedToken
-isDispenserContract
-isFixedRateContract
-opcCollector
-providerFee
-routerOwner
-swapNonOceanFee
-swapOceanFee
-updateOPCCollector
-updateOPCFee
-"""
 class FactoryRouter(ContractBase):
     CONTRACT_NAME = "FactoryRouter"
