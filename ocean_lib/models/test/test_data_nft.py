2728d56b67389e0d532cc45d1a5892133fa128ee
Maria Carmina <mariacarmina.cretu@gmail.com>
2023-01-13 11:34:48 +0200

    Found fix and caught exception.

--- ocean_lib/models/test/test_data_nft.py
+++ ocean_lib/models/test/test_data_nft.py
@@ -1,28 +1,25 @@
 import pytest
+from brownie.network import web3
 
 
 @pytest.mark.unit
 def test_fail_transfer_function(consumer_wallet, publisher_wallet, config, data_nft):
     """Tests failure of using the transfer functions."""
     with pytest.raises(
-        Exception,
-        match="transfer caller is not owner nor approved",
+        ValueError,
+        match="VM Exception while processing transaction: revert ERC721: transfer caller is not owner nor approved",
     ):
-        data_nft.transferFrom(
+        tx = data_nft.transferFrom(
             publisher_wallet.address,
             consumer_wallet.address,
             1,
             {"from": consumer_wallet, "required_confs": 0},
         )
-
-    # # Tests for safe transfer as well
-    # with pytest.raises(
-    #     Exception,
-    #     match="transfer caller is not owner nor approved",
-    # ):
-    #     data_nft.safeTransferFrom(
-    #         publisher_wallet.address,
-    #         consumer_wallet.address,
-    #         1,
-    #         {"from": consumer_wallet},
-    #     )
+        tx.wait(1)
+        replay_tx = {
+            "to": tx.receiver,
+            "from": tx.sender.address,
+            "value": tx.value,
+            "data": tx.input,
+        }
+        web3.eth.call(replay_tx, tx.block_number - 1)
