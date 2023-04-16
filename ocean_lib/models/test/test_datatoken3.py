9b64bda907515c2823f7561b749a699cc7d87539
trentmc <gtrent@gmail.com>
2023-04-16 13:45:36 +0900

    first cut in test of OCEAN staking with DT

--- ocean_lib/models/test/test_datatoken3.py
+++ ocean_lib/models/test/test_datatoken3.py
@@ -36,34 +36,23 @@ Q: how to reconcile DNFT/DT with "distribute $ from feed?
 Remaining challenges:
 - privacy for submitted predvals, agg predval, when computing agg predval. See Product Design GDoc. 
 """
-
+import os
+    
 import brownie
+from brownie.network import accounts
 import pytest
 from pytest import approx
 
-from util.base18 import from_wei, to_wei
-from util.constants import BROWNIE_PROJECT as B
-from util.predictoor import PyPredictoorContract
+    
+from ocean_lib.example_config import get_config_dict
+from ocean_lib.web3_internal.utils import connect_to_network
+from ocean_lib.ocean.ocean import Ocean
+from ocean_lib.ocean.mint_fake_ocean import mint_fake_OCEAN
+from ocean_lib.ocean.util import from_wei, to_wei
 
 accounts = brownie.network.accounts
 chain = brownie.network.chain
 predictoor1, predictoor2, trader, opf, rando, contract_account = accounts[:6]
-
-def test_init_py():
-    _test_init(use_py=True)
-
-@pytest.mark.skip(reason="turn this on once .sol version is built")
-def test_init_sol():
-    _test_init(use_py=False)
-
-def _test_init(use_py):
-    OCEAN = _deployOCEAN(opf)
-    pe_contract = _predictoor_contract(
-        use_py, 5, OCEAN, to_wei(100.1), {"from": opf}, contract_account)
-    assert pe_contract.address == contract_account.address 
-    assert pe_contract.min_blocks_ahead() == 5
-    assert pe_contract.pmt_token().address == OCEAN.address
-    assert from_wei(pe_contract.pmt_amt_wei()) == 100.1
 
 def test_main_py():
     _test_main(use_py=True)
@@ -73,6 +62,46 @@ def test_main_sol():
     _test_main(use_py=False)
     
 def _test_main(use_py):
+
+    #======
+    #SETUP SYSTEM
+    connect_to_network("development")
+    config = get_config_dict("development")
+    ocean = Ocean(config)
+
+    # Create OCEAN object. Barge auto-created OCEAN, and ocean instance knows
+    OCEAN = ocean.OCEAN_token
+
+    # Create Alice's wallet
+    alice_private_key = os.getenv("TEST_PRIVATE_KEY1")
+    alice = accounts.add(alice_private_key)
+
+    print("Test me: does Alice have OCEAN at this point? If no, ok too")
+    import pdb; db.set_trace() 
+    assert alice.balance() > 0, "Alice needs ETH"
+    assert OCEAN.balanceOf(alice) > 0, "Alice needs OCEAN"
+
+    print("We want opf account = alice account, with $$. How?")
+    print(f"  alice.address = {alice.address}")
+    for i, account in enumerate(accounts[6:]):
+        print(f"  accounts[i].address = {account.address}")
+    import pdb; db.set_trace()
+
+    print(f"Do I have private keys? Deployer does minting, key1 is alice")
+    for key_label in ["FACTORY_DEPLOYER_PRIVATE_KEY", "TEST_PRIVATE_KEY1", "TEST_PRIVATE_KEY2", "TEST_PRIVATE_KEY3"]:
+        key = os.environ.get(key_label)
+        print(f"  {key_label} = {key}")
+    import pdb; db.set_trace()
+
+    # Mint fake OCEAN to envvar TEST_PRIVATE_KEY{1,2,3}. Alice is key 1.
+    mint_fake_OCEAN(config)
+
+    #give OCEAN to key users
+    initbal = 100.0
+    OCEAN.transfer(predictoor1, to_wei(initbal), {"from": alice})
+    OCEAN.transfer(predictoor2, to_wei(initbal), {"from": alice})
+    OCEAN.transfer(trader, to_wei(initbal), {"from": alice})
+    
     #======================================================================
     #deploy OCEAN, give some to key users
     OCEAN = _deployOCEAN(opf)
@@ -83,132 +112,112 @@ def _test_main(use_py):
 
     #convenience functions to get OCEAN balance & allowance
     c = ConvClass(OCEAN)
-    getbal = c.fromWei_balanceOf
-    getallow = c.fromWei_allowance
-    approve = c.toWei_approve
+    OCEAN_bal = c.fromWei_balanceOf
+    OCEAN_approved = c.fromWei_approved
 
     #======================================================================
-    #deploy contract
-    num_blocks_ahead = 20 # Pred'ns must be >= this # blocks ahead
-    num_blocks_before_release = 100 # time til funds releasable
-    pmt_amt = 100.0 # price
-    pe_contract = _predictoor_contract(
-        use_py = use_py,
-        min_blocks_ahead = 5, # < num_blocks_head because we need txs to test
-        num_blocks_before_release = num_blocks_before_release,
-        pmt_token = OCEAN,
-        pmt_amt_wei = to_wei(pmt_amt),
-        tx_dict = {"from":opf},
-        contract_account = contract_account
+    #SETUP CONTRACTS
+
+    min_blocks_ahead = 20 # Pred'ns must be >= this # blocks ahead
+    min_predns_for_payout = 100 # Predictoor must have >= this # predn's to get paid
+    num_blocks_subscription = 86400 / 10 # 1 day
+
+    #can reuse existing DNFT template
+    data_nft = ocean.data_nft_factory.create(
+        {"from": opf}, "Data NFT 1", "DNFT1", #and any other DataNFTArguments
     )
-    predict_blocknum =  _cur_blocknum() + num_blocks_ahead
+
+    #new DT template with Predictoor functionality
+    DT = data_nft.create_datatoken(
+        {"from": opf}, name="Datatoken 1", symbol="DT1",
+        template_index=3,
+        min_blocks_ahead=min_blocks_ahead,
+        min_predns_for_payout=min_predns_for_payout,
+        num_blocks_subscription=num_blocks_subscription,
+        #and any other DatatokenArguments
+    )
+
+    #post 100 DTs for sale
+    n_DTs = 100.0
+    DT.mint(opf, to_wei(n_DTs), {"from": opf})
+
+    DT_price = 10.0 #denominated in OCEAN
+    exchange = DT.create_exchange(
+        {"from": opf}, to_wei(pmt_amount), OCEAN.address)
+    DT.approve(exchange.address, to_wei(n_DTs), {"from": opf})
+
+    predictoors = [predictoor1, predictoor2]
 
     #======================================================================
-    #predictoors submit predictions
+    #TRADER BUYS SUBSCRIPTION TO AGG PREDVALS (do every 24h)
+
+    # trader buys 1 DT with OCEAN
+    OCEAN_needed = exchange.BT_needed(to_wei(1), consume_market_fee=0)
+    OCEAN.approve(exchange.address, to_wei(OCEAN_needed), {"from": trader})
+    exchange.buy_DT(to_wei(1), {"from": trader}) #spends OCEAN
+
+    # trader starts subscription. Good for 24h.
+    # -"start_subscription" is like pay_for_access_service, but w/o DDO
+    DT.approve(DT, to_wei(1), {"from": trader})
+    DT.start_subscription({"from": trader}) # good for next 24h (or 1h, 7d, ..)
+
+    #======================================================================
+    #PREDICTOORS STAKE & SUBMIT PREDVALS (do every 5min)
     predval1, stake1 = 1500, 10.0    
     predval2, stake2 = 1600, 20.0
 
-    approve(pe_contract, stake1, predictoor1)
-    approve(pe_contract, stake2, predictoor2)
-    assert getallow(predictoor1, pe_contract) == stake1 #new, ready to use
-    assert getallow(predictoor2, pe_contract) == stake2 #""
-    
-    pe_contract.submit_predval(
-        to_wei(predval1), to_wei(stake1), predict_blocknum,{"from":predictoor1})
-    pe_contract.submit_predval(
-        to_wei(predval2), to_wei(stake2), predict_blocknum,{"from":predictoor2})
+    OCEAN.approve(DT, to_wei(stake1), predictoor1)
+    OCEAN.approve(DT, to_wei(stake2), predictoor2)
+    assert OCEAN_approved(predictoor1, DT) == stake1 #new, ready to use
+    assert OCEAN_approved(predictoor2, DT) == stake2 #""
+
+    DT.submit_predval(to_wei(predval1), to_wei(stake1), predict_blocknum, {"from":predictoor1})
+    DT.submit_predval(to_wei(predval1), to_wei(stake2), predict_blocknum, {"from":predictoor2})
 
     #test
-    assert getbal(pe_contract) == (stake1 + stake2)  #just got new stake
-    assert getbal(predictoor1) == (initbal - stake1) #just staked
-    assert getbal(predictoor2) == (initbal - stake2) #""
-    assert getallow(predictoor1, pe_contract) == 0.0 #just used up
-    assert getallow(predictoor2, pe_contract) == 0.0 #""
-
+    assert OCEAN_bal(DT) == (stake1 + stake2)  #just got new stake
+    assert OCEAN_bal(predictoor1) == (initbal - stake1) #just staked
+    assert OCEAN_bal(predictoor2) == (initbal - stake2) #""
+    assert OCEAN_approved(predictoor1, DT) == 0.0 #just used up
+    assert OCEAN_approved(predictoor2, DT) == 0.0 #""
 
     #======================================================================
-    #trader gets aggregated prediction value, for a price
-    assert getbal(trader) == initbal
-    
-    approve(pe_contract, pmt_amt, trader)
-    assert getallow(trader, pe_contract) == pmt_amt #new
-    
-    agg_predval_wei = pe_contract.get_agg_predval(
-        predict_blocknum, {"from": trader})
-    assert agg_predval_wei is not None, "we expected predictions here"
-    
-    agg_predval = from_wei(agg_predval_wei)
-    assert agg_predval == (10.0*1500 + 20.0*1600) / (10.0 + 20.0)
+    #TRADER GETS AGG PREDVAL (do every 5min)
 
-    #test
-    assert getbal(pe_contract) == (stake1 + stake2 + pmt_amt) #just got pmt_amt
-    assert getbal(predictoor1) == (initbal - stake1) #no change
-    assert getbal(predictoor2) == (initbal - stake2) #""
-    assert getbal(trader) == (initbal - pmt_amt)     #just spent pmt_amt
-    
-    assert getallow(predictoor1, pe_contract) == 0.0 #no change
-    assert getallow(predictoor2, pe_contract) == 0.0 #"
-    assert getallow(trader, pe_contract) == 0.0      #just used up
-
+    # trader gets prediction itself
+    # -a tx isn't needed, therefore Solidity can return a value directly
+    # -"get_agg_predval" is like ocean.assets.download_asset //
+    #  asset_downloader.download_asset_files() except there's no downloading,
+    #  rather it simply gives the agg_predval. (To generalize: reveals secret)
+    agg_predval_wei = DT.get_agg_predval(predict_blocknum)
     
     #======================================================================
-    #time passes
-    chain.mine(num_blocks_ahead + 10) # pass enough time (blocks) so that  pass
+    #TIME PASSES - enough such that predict_blocknum has passed
+    chain.mine(min_blocks_ahead + 10) # pass enough time (blocks) so that  pass
 
-    
     #======================================================================
-    #owner submits true value. This will update predictoors' claimable amts
+    #OWNER SUBMITS TRUE VALUE. This will update predictoors' claimable amts
     trueval = 1612.0
-    pe_contract.submit_trueval(
-        to_wei(trueval), predict_blocknum, {"from":opf})
-
-    #none of the balances or allowances should have changed. Must wait!
-    assert getbal(pe_contract) == (stake1 + stake2 + pmt_amt) #no change
-    assert getbal(predictoor1) == (initbal - stake1) #no change
-    assert getbal(predictoor2) == (initbal - stake2) #""
-    
-    assert getallow(predictoor1, pe_contract) == 0.0 #no change
-    assert getallow(predictoor2, pe_contract) == 0.0 #"
-    assert getallow(trader, pe_contract) == 0.0      #"
+    DT.submit_trueval(to_wei(trueval), predict_blocknum, {"from":opf})
     
     #======================================================================
-    #let time pass, then release to update predictoors' claimable amts
-    chain.mine(num_blocks_before_release)
-    pe_contract.release({"from": rando})
+    #TIME PASSES - enough for predictoors to get claims
 
-    exp_pmt1 = 100.1 * 10.0/(10.0+20.0)
-    exp_pmt2 = 100.1 * 20.0/(10.0+20.0)
-
-    #test
-    assert getbal(pe_contract) == (stake1 + stake2 + pmt_amt) #no change
-    assert getbal(predictoor1) == (initbal - stake1) #no change
-    assert getbal(predictoor2) == (initbal - stake2) #""
-
-    assert getallow(pe_contract,predictoor1)==approx(exp_pmt1+stake1,rel=0.1)
-    assert getallow(pe_contract,predictoor2)==approx(exp_pmt2+stake2,rel=0.1)
-
+    # FIXME - we'll need to do 'min_predns_for_payout' loops through this
+    #   step and and several prev steps, for predictoors to actually get paid
+    chain.mine(int(1.2 * min_blocks_ahead * min_predns_for_payout))
 
     #======================================================================
-    #predictoors claim their $
-    allow1 = getallow(pe_contract, predictoor1)
-    allow2 = getallow(pe_contract, predictoor2)
-    
-    OCEAN.transferFrom(
-        pe_contract.address, predictoor1.address, to_wei(allow1),
-        {"from": predictoor1})
-    OCEAN.transferFrom(
-        pe_contract.address, predictoor2.address, to_wei(allow2),
-        {"from": predictoor2})
+    #PREDICTOORS & OPF COLLECT SALES REVENUE
 
-    #test
-    assert getbal(pe_contract) == approx(0.0)
-    assert getbal(predictoor1) == \
-        approx(initbal - stake1 + exp_pmt1 + stake1, rel=0.1) # pmt + stake back
-    assert getbal(predictoor2) == \
-        approx(initbal - stake2 + exp_pmt2 + stake2, rel=0.1) # pmt + stake back
-    
-    assert getallow(pe_contract, predictoor1) == approx(0.0) #just used up
-    assert getallow(pe_contract, predictoor2) == approx(0.0) #""
+    # Any rando can call release(). Will update amt allowed
+    # OR!!! this is where exchange object comes in!!
+    DT.release({"from": rando})
+
+    # Each actor collects its cut of DT sales
+    for acct in predictoors + [opf]:
+        amt = getallow(DT, acct)
+        OCEAN.transferFrom(DT.address, acct.address, to_wei(amt), {"from": acct})
 
 
 class ConvClass: 
@@ -218,11 +227,8 @@ class ConvClass:
     def fromWei_balanceOf(self, obj) -> float:
         return from_wei(self.token.balanceOf(obj.address))
 
-    def fromWei_allowance(self, obj1, obj2) -> float:
+    def fromWei_approved(self, obj1, obj2) -> float:
         return from_wei(self.token.allowance(obj1.address, obj2.address))
-
-    def toWei_approve(self, to_obj, amt:float, from_obj) -> float:
-        self.token.approve(to_obj.address, to_wei(amt), {"from":from_obj})
 
 def _cur_blocknum():
     return chain[-1].number
@@ -232,7 +238,7 @@ def _predictoor_contract(
         min_blocks_ahead:int,
         num_blocks_before_release:int,
         pmt_token,
-        pmt_amt_wei:int,
+        DT_price_wei:int,
         tx_dict:dict,
         contract_account):
 
@@ -241,7 +247,7 @@ def _predictoor_contract(
             min_blocks_ahead,
             num_blocks_before_release,
             pmt_token,
-            pmt_amt_wei,
+            DT_price_wei,
             tx_dict,
             contract_account)
 
