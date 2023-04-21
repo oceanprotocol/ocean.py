76d72cbc5762bdbf8b581a19b9424639dfdb291f
trizin <25263018+trizin@users.noreply.github.com>
2023-04-21 15:27:07 +0000

    Finalize prototype template

--- ocean_lib/models/predictoor/datatoken3.py
+++ ocean_lib/models/predictoor/datatoken3.py
@@ -14,6 +14,8 @@ class Prediction:
     def __init__(self, prediction, stake):
         self.prediction = prediction
         self.stake = stake
+        self.score = 0
+        self.paid = False
 
 
 class Datatoken3(Datatoken1):
@@ -23,23 +25,66 @@ class Datatoken3(Datatoken1):
         super().__init__(config_dict, address)
         self.subscribers = {}  # [address] = timestamp
         self.predictions = {}  # [predict_blocknum][address] = Prediction
+        self.trueval = {}  # [blocknum] = trueval
+
+        self.prediction_counter = {}  # [blocknum] = counter
+        self.stake_counter = {}  # [blocknum] = counter
 
     # placeholders for now
     def submit_predval(self, token, prediction, stake, predict_blocknum, tx_dict):
         prediction = Prediction(prediction, stake)
+
         if predict_blocknum not in self.predictions:
             self.predictions[predict_blocknum] = {}
+            self.prediction_counter[predict_blocknum] = 0
+            self.stake_counter[predict_blocknum] = 0
+
         self.predictions[predict_blocknum][tx_dict["from"]] = prediction
-        token.transferFrom(
+
+        assert (token.transferFrom(
             tx_dict["from"], self.address, stake, {"from": self.address}
-        )
+        ))
+
+        self.prediction_counter[predict_blocknum] += 1
+        self.stake_counter[predict_blocknum] += stake
 
     def get_agg_predval(self, stake, blocknum):
         pass
 
-    def submit_trueval_and_payout(self, blocknum, prediction):
-        pass
+    def submit_trueval(self, blocknum, trueval, tx_dict):
+        # assert sender == opf
+        self.trueval[blocknum] = trueval
+
+        # calc accuracy for each prediction
+        inverse_sum = 0
+        pred_inv = {}
+        for addr in self.predictions[blocknum]:
+            prediction = self.predictions[blocknum][addr]
+            inverse_sum += 1 / prediction.prediction
+            pred_inv[addr] = 1 / prediction.prediction
+
+        for addr in self.predictions[blocknum]:
+            prediction = self.predictions[blocknum][addr]
+            prediction.score = pred_inv[addr] / inverse_sum
 
     def start_subscription(self, tx_dict):
         if tx_dict["from"] not in self.subscribers:
             self.subscribers[tx_dict["from"]] = 100  # mock timestamp
+
+    def get_agg_predval(self, blocknum):
+        s = 0
+        w = 0
+        for addr in self.predictions[blocknum]:
+            prediction = self.predictions[blocknum][addr]
+            s += prediction.prediction * prediction.stake
+            w += prediction.stake
+        return s / w
+
+    def get_payout(self, blocknum, OCEAN, predictoor_addr, tx_dict):
+        assert (self.predictions[blocknum][predictoor_addr].paid == False)
+
+        amt = self.predictions[blocknum][predictoor_addr].score * self.stake_counter[blocknum]
+        if OCEAN.balanceOf(self.address) < amt:  # precision loss
+            amt = OCEAN.balanceOf(self.address)
+        assert (OCEAN.transfer(predictoor_addr, amt, {"from": self.address}))
+        self.predictions[blocknum][predictoor_addr].paid = True
