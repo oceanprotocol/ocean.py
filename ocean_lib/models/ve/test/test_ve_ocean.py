770019048ccf4435ea381212a06da3d3eb9d9bf7
Calina Cenan <calina@cenan.net>
2023-08-04 05:31:12 +0000

    Fix test veocean.

--- ocean_lib/models/ve/test/test_ve_ocean.py
+++ ocean_lib/models/ve/test/test_ve_ocean.py
@@ -3,7 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 import pytest
-from eth_account.account import Account
 
 from ocean_lib.ocean.util import from_wei, send_ether, to_wei
 
@@ -32,21 +31,22 @@ def test_ve_ocean1(ocean, factory_deployer_wallet, ocean_token):
     TA = to_wei(0.0001)
     OCEAN.mint(alice_wallet.address, TA, {"from": factory_deployer_wallet})
 
-    veOCEAN.checkpoint({"from": factory_deployer_wallet})
+    latest_block = web3.eth.getBlock("latest")
+    veOCEAN.checkpoint({"from": factory_deployer_wallet, "gas": latest_block.gasLimit})
     OCEAN.approve(veOCEAN.address, TA, {"from": alice_wallet})
 
     latest_block = ocean.config_dict["web3_instance"].eth.get_block("latest")
     t0 = latest_block.timestamp  # ve funcs use block.timestamp, not chain.time()
     t1 = t0 // WEEK * WEEK + WEEK  # this is a Thursday, because Jan 1 1970 was
     t2 = t1 + WEEK
-    # TODO: mining??
-    chain.sleep(t1 - t0)
+
+    provider = web3.provider
+    provider.make_request("evm_increaseTime", [(t1 - t0)])
 
     assert OCEAN.balanceOf(alice_wallet.address) != 0
 
-    veOCEAN.create_lock(
-        TA, t2, {"from": alice_wallet, "gas_limit": chain.block_gas_limit}
-    )
+    latest_block = web3.eth.getBlock("latest")
+    veOCEAN.create_lock(TA, t2, {"from": alice_wallet, "gas": latest_block.gasLimit})
 
     assert OCEAN.balanceOf(alice_wallet.address) == 0
 
@@ -55,17 +55,20 @@ def test_ve_ocean1(ocean, factory_deployer_wallet, ocean_token):
 
     assert veOCEAN.get_last_user_slope(alice_wallet) != 0
 
+    latest_block = web3.eth.getBlock("latest")
     alice_vote_power = float(
-        from_wei(veOCEAN.balanceOf(alice_wallet, chain[-1].timestamp))
+        from_wei(veOCEAN.balanceOf(alice_wallet, latest_block.timestamp))
     )
     expected_vote_power = float(from_wei(TA)) * WEEK / MAXTIME
     assert alice_vote_power == pytest.approx(expected_vote_power, TA / 20.0)
 
-    brownie.network.chain.sleep(t2)
-    chain.mine()
+    provider.make_request("evm_increaseTime", [t2])
+    provider.make_request("evm_mine", [])
 
-    veOCEAN.withdraw({"from": alice_wallet, "gas_limit": chain.block_gas_limit})
+    latest_block = web3.eth.getBlock("latest")
+    veOCEAN.withdraw({"from": alice_wallet, "gas": latest_block.gasLimit})
     assert OCEAN.balanceOf(alice_wallet.address) == TA
 
+    latest_block = web3.eth.getBlock("latest")
     assert veOCEAN.get_last_user_slope(alice_wallet) == 0
-    assert veOCEAN.balanceOf(alice_wallet, chain.time()) == 0
+    assert veOCEAN.balanceOf(alice_wallet, latest_block.timestamp) == 0
